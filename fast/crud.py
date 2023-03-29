6d9084b41ab6d3e52e881cb5500f2cb0d800452c
Inna Kushnir <innakushnir63@gmail.com>
2023-03-29 21:53:05 +0300

    Solution

---
+++ fast/crud.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from sqlalchemy.orm import Session
+from db import models
+import schemas
+
+
+def get_all_author(
+        db: Session,
+        skip: int = 0, limit: int = 100,):
+
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_name(db: Session, name: str):
+    return(
+        db.query(models.Author).filter(
+            models.Author.name == name).first()
+    )
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(
+        name=author.name,
+        bio=author.bio
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def get_author(db: Session, author_id: int):
+    return db.query(models.Author).filter(
+        models.Author.id == author_id).first()
+
+
+def get_book_list(
+    db: Session,
+    skip: int = 0, limit: int = 100,
+    author_id: int | None = None,
+):
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(
+            models.Book.author_id == author_id)
+    queryset = queryset.offset(skip).limit(limit)
+
+    return queryset.all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
---
+++ fast/db/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from db.engine import Base
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    bio = Column(String(255), nullable=False)
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(255), nullable=False)
+    publication_date = Column(Date(), nullable=False)
+    author_id = Column(Integer, ForeignKey("author.id"))
+    author = relationship("Author", back_populates="books")
