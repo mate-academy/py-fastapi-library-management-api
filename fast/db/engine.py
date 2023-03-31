816e39e326bb022a7fa114926e21f7aa4776e717
Inna Kushnir <innakushnir63@gmail.com>
2023-03-31 17:47:43 +0300

    Solution

--- fast/db/engine.py
+++ fast/db/engine.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fast.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fast/fast.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(
