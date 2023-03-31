0106ea32407a9ae0604043eee79a1bb44076a377
IvanGLS <gladiolus0707@gmail.com>
2023-03-31 19:22:24 +0300

    Solution

--- crud.py
+++ crud.py
@@ -1,54 +1,55 @@
-import schemas
+from typing import List, Optional
 from sqlalchemy.orm import Session
 from database import models
+import schemas
 
 
-def get_author(db: Session, author_id: int) -> models.Author:
-    return db.query(models.Author).filter(models.Author.id == author_id).first()
+class BookService:
+    def __init__(self, db: Session):
+        self.db = db
 
+    def get_author(self, author_id: int) -> Optional[models.Author]:
+        return self.db.query(models.Author).filter(models.Author.id == author_id).first()
 
-def get_authors(db: Session, skip: int = 0, limit: int = 100) -> list[models.Author]:
-    return db.query(models.Author).offset(skip).limit(limit).all()
+    def get_authors(self, skip: int = 0, limit: int = 100) -> List[models.Author]:
+        return self.db.query(models.Author).offset(skip).limit(limit).all()
 
+    def create_author(self, author: schemas.AuthorCreate) -> models.Author:
+        db_author = models.Author(name=author.name, bio=author.bio)
+        self.db.add(db_author)
+        self.db.commit()
+        self.db.refresh(db_author)
+        return db_author
 
-def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
-    db_author = models.Author(name=author.name, bio=author.bio)
-    db.add(db_author)
-    db.commit()
-    db.refresh(db_author)
-    return db_author
+    def get_author_by_name(self, name: str) -> Optional[models.Author]:
+        return self.db.query(models.Author).filter(models.Author.name == name).first()
 
+    def get_book(self, book_id: int) -> Optional[models.Book]:
+        return self.db.query(models.Book).filter(models.Book.id == book_id).first()
 
-def get_author_by_name(db: Session, name: str) -> models.Author:
-    return db.query(models.Author).filter(models.Author.name == name).first()
+    def get_books(self, skip: int = 0, limit: int = 100) -> List[models.Book]:
+        return self.db.query(models.Book).offset(skip).limit(limit).all()
 
+    def get_books_by_author(self, author_id: int, skip: int = 0, limit: int = 100) -> List[models.Book]:
+        return (
+            self.db.query(models.Book)
+            .filter(models.Book.author_id == author_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
 
-def get_book(db: Session, book_id: int) -> models.Book:
-    return db.query(models.Book).filter(models.Book.id == book_id).first()
+    def create_book(self, book: schemas.BookCreate) -> models.Book:
+        db_book = models.Book(
+            title=book.title,
+            summary=book.summary,
+            publication_date=book.publication_date,
+            author_id=book.author_id,
+        )
+        self.db.add(db_book)
+        self.db.commit()
+        self.db.refresh(db_book)
+        return db_book
 
-
-def get_books(db: Session, skip: int = 0, limit: int = 100) -> list[models.Book]:
-    return db.query(models.Book).offset(skip).limit(limit).all()
-
-
-def get_books_by_author(db: Session, author_id: int, skip: int = 0, limit: int = 100) -> list[models.Book]:
-    return (
-        db.query(models.Book)
-        .filter(models.Book.author_id == author_id)
-        .offset(skip)
-        .limit(limit)
-        .all()
-    )
-
-
-def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
-    db_book = models.Book(
-        title=book.title,
-        summary=book.summary,
-        publication_date=book.publication_date,
-        author_id=book.author_id,
-    )
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
+    def get_book_by_title(self, title: str) -> Optional[models.Book]:
+        return self.db.query(models.Book).filter(models.Book.title == title).first()
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from typing import List, Optional
 
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+
 from database.engine import SessionLocal
 import schemas
-import crud
+from crud import BookService
 
 # define FastAPI application object
 app = FastAPI()
@@ -22,23 +23,26 @@ def get_db() -> Session:
 # API endpoint to create a new author
 @app.post("/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)) -> schemas.Author:
-    db_author = crud.get_author_by_name(db, name=author.name)
+    book_service = BookService(db)
+    db_author = book_service.get_author_by_name(name=author.name)
     if db_author:
         raise HTTPException(status_code=400, detail="Author name already registered")
-    return crud.create_author(db=db, author=author)
+    return book_service.create_author(author=author)
 
 
 # API endpoint to retrieve a list of authors with pagination (skip, limit)
 @app.get("/authors/", response_model=list[schemas.Author])
 def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> List[schemas.Author]:
-    authors = crud.get_authors(db, skip=skip, limit=limit)
+    book_service = BookService(db)
+    authors = book_service.get_authors(skip=skip, limit=limit)
     return authors
 
 
 # API endpoint to retrieve a single author by ID
 @app.get("/authors/{author_id}", response_model=schemas.Author)
 def read_author(author_id: int, db: Session = Depends(get_db)) -> Optional[schemas.Author]:
-    db_author = crud.get_author(db, author_id=author_id)
+    book_service = BookService(db)
+    db_author = book_service.get_author(author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
     return db_author
@@ -49,13 +53,18 @@ def read_author(author_id: int, db: Session = Depends(get_db)) -> Optional[schem
 def create_book_for_author(
     book: schemas.BookCreate, db: Session = Depends(get_db),
 ) -> schemas.Book:
-    return crud.create_book(db=db, book=book)
+    book_service = BookService(db)
+    author = book_service.get_author(author_id=book.author_id)
+    if author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return book_service.create_book(book=book)
 
 
 # API endpoint to retrieve a list of books with pagination (skip, limit)
 @app.get("/books/", response_model=list[schemas.Book])
 def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> List[schemas.Book]:
-    books = crud.get_books(db, skip=skip, limit=limit)
+    book_service = BookService(db)
+    books = book_service.get_books(skip=skip, limit=limit)
     return books
 
 
@@ -64,7 +73,8 @@ def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -
 def read_books_by_author(
     author_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 ) -> List[schemas.Book]:
-    books = crud.get_books_by_author(db, author_id=author_id, skip=skip, limit=limit)
+    book_service = BookService(db)
+    books = book_service.get_books_by_author(author_id=author_id, skip=skip, limit=limit)
     if not books:
         raise HTTPException(status_code=404, detail="Author has no books")
     return books
