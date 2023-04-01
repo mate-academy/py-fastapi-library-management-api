172da15dbb850983344a7d5d5e8fdb9e324ed775
Inna Kushnir <innakushnir63@gmail.com>
2023-04-01 15:20:36 +0300

    Solution BookService

--- crud.py
+++ crud.py
@@ -1,48 +1,65 @@
-from __future__ import annotations
+from typing import List, Union
+from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
-import schemas
+from database import SessionLocal, engine
 import models
 
-
-def get_all_author(    db: Session,
-    skip: int = 0,    limit: int = 100,
-):
-    return db.query(models.Author).offset(skip).limit(limit).all()
+from pydantic import BaseModel
+from datetime import date
+from schemas import AuthorCreate, BookCreate
 
 
-def get_author_by_name(db: Session, name: str):
-    return db.query(models.Author).filter(models.Author.name == name).first()
+class DBSession:
+    def __init__(self):
+        self._session = None
+    def __enter__(self):
+        self._session = SessionLocal()
+        return self._session
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._session.close()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
-    db_author = models.Author(name=author.name, bio=author.bio)
-    db.add(db_author)
-    db.commit()
-    db.refresh(db_author)
-    return db_author
-
-def get_author(db: Session, author_id: int):
-    return db.query(models.Author).filter(models.Author.id == author_id).first()
+def get_db() -> Session:
+    with DBSession() as db:
+        yield db
 
 
-def get_book_list(
-    db: Session,    skip: int = 0,
-    limit: int = 100,    author_id: int | None = None,
-):
-    queryset = db.query(models.Book)
-    if author_id is not None:
-        queryset = queryset.filter(models.Book.author_id == author_id)
-        queryset = queryset.offset(skip).limit(limit)
-    return queryset.all()
+class BookService:
+    def __init__(self, db: Session = Depends(get_db)):
+        self.db = db
 
-def create_book(db: Session, book: schemas.BookCreate):
-    db_book = models.Book(
-        title=book.title,
-        summary=book.summary,
-        publication_date=book.publication_date,
-        author_id=book.author_id,
-    )
-    db.add(db_book)
-    db.commit()
-    db.refresh(db_book)
-    return db_book
+    def get_all_author(
+            self,
+            skip: int = 0,
+            limit: int = 100,
+    ) -> List[models.Author]:
+        return self.db.query(models.Author).offset(skip).limit(limit).all()
+
+    def create_author(self, author: AuthorCreate):
+        db_author = models.Author(name=author.name, bio=author.bio)
+        self.db.add(db_author)
+        self.db.commit()
+        self.db.refresh(db_author)
+        return db_author
+
+    def get_author(self, author_id: int):
+        return self.db.query(models.Author).filter(models.Author.id == author_id).first()
+
+    def get_books(self, author_id: Union[int, None] = None,  skip: int = 0, limit: int = 100, ) -> List[models.Book]:
+        queryset = self.db.query(models.Book)
+        if author_id is not None:
+            queryset = queryset.filter(models.Book.author_id == author_id)
+        return queryset.offset(skip).limit(limit).all()
+
+    def create_book(self, book: BookCreate):
+        db_book = models.Book(
+            title=book.title,
+            summary=book.summary,
+            publication_date=book.publication_date,
+            author_id=book.author_id,
+        )
+        self.db.add(db_book)
+        self.db.commit()
+        self.db.refresh(db_book)
+        return db_book
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Union
+from typing import Union, List
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 import crud
@@ -9,44 +9,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-class DBSession:
-    def __init__(self):
-        self._session = None
-    def __enter__(self):
-        self._session = SessionLocal()
-        return self._session
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._session.close()
-
-def get_db() -> Session:
-    with DBSession() as db:
-        yield db
 
 @app.get("/")
-def root() -> dict:
+async def root():
     return {"message": "Hello World"}
 
-@app.get("/authors/", response_model=list[schemas.Author])
-def read_author(db: Session = Depends(get_db)):
-    return crud.get_all_author(db=db)
+
+@app.get("/authors/", response_model=List[schemas.Author])
+def get_author(book_service: crud.BookService = Depends()):
+    return book_service.get_all_author()
+
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
-    db_author = crud.get_author_by_name(db=db, name=author.name)
-    if db_author:
-        raise HTTPException(status_code=400, detail="Such author already exists")
-    return crud.create_author(db=db, author=author)
+def create_author(author: schemas.AuthorCreate, book_service: crud.BookService = Depends()):
+
+    return book_service.create_author(author=author)
+
+
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def read_single_author(author_id: Union[int, None]=None, db: Session = Depends(get_db)):
-    db_author = crud.get_author(db=db, author_id=author_id)
+def read_single_author(author_id: Union[int, None] = None, book_service: crud.BookService = Depends()):
+    db_author = book_service.get_author(author_id=author_id)
+
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
+
     return db_author
 
-@app.get("/books/", response_model=list[schemas.Book])
-def read_book(author_id: int = None, db: Session = Depends(get_db)):
-    return crud.get_book_list(db=db, author_id=author_id)
+
+@app.get("/books/", response_model=List[schemas.Book])
+def get_books(author_id: Union[int, None] = None, book_service: crud.BookService = Depends()):
+    return book_service.get_books(author_id=author_id)
+
 
 @app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    return crud.create_book(db=db, book=book)
+def create_author(book: schemas.BookCreate, book_service: crud.BookService = Depends()):
+
+    return book_service.create_book(book=book)
+
