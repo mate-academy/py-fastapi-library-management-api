9b6bdb127b618ce20063b826c20afc834a3912a2
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-04-04 22:22:34 +0300

    Implement simple library API

---
+++ crud.py
@@ -0,0 +1,63 @@
+from sqlalchemy.orm import Session, Query
+
+import models
+import schemas
+
+
+def create_author(
+        db: Session,
+        author: schemas.AuthorCreate
+) -> models.DBAuthor:
+    db_author = models.DBAuthor(
+        name=author.name,
+        bio=author.bio
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_authors_list(
+        db: Session,
+        skip: int = 0,
+        limit: int = 100
+) -> Query:
+    return db.query(models.DBAuthor).offset(skip).limit(limit).all()
+
+
+def get_author(db: Session, author_id: int) -> Query:
+    return db.query(models.DBAuthor).filter(
+        models.DBAuthor.id == author_id
+    ).first()
+
+
+def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
+    db_book = models.DBBook(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
+
+
+def get_books_list(db: Session, skip: int = 0, limit: int = 100) -> Query:
+    return db.query(models.DBBook).offset(skip).limit(limit).all()
+
+
+def get_books_by_author_id(db: Session, author_id: int):
+    return db.query(models.DBBook).filter(
+        models.DBBook.author_id == author_id
+    ).all()
+
+
+def get_author_by_name(db: Session, name: str) -> Query:
+    return (
+        db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+    )
