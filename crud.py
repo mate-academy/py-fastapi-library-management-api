45c95d237573b871237c1fcbee3ac28215a46618
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-04-05 15:35:26 +0300

    Return types correction

--- crud.py
+++ crud.py
@@ -23,11 +23,11 @@ def get_authors_list(
         db: Session,
         skip: int = 0,
         limit: int = 100
-) -> Query:
+) -> list[models.DBAuthor]:
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
-def get_author(db: Session, author_id: int) -> Query:
+def get_author(db: Session, author_id: int) -> models.DBAuthor | None:
     return db.query(models.DBAuthor).filter(
         models.DBAuthor.id == author_id
     ).first()
@@ -47,17 +47,17 @@ def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
     return db_book
 
 
-def get_books_list(db: Session, skip: int = 0, limit: int = 100) -> Query:
+def get_books_list(db: Session, skip: int = 0, limit: int = 100) -> list[models.DBBook]:
     return db.query(models.DBBook).offset(skip).limit(limit).all()
 
 
-def get_books_by_author_id(db: Session, author_id: int):
+def get_books_by_author_id(db: Session, author_id: int) -> list[models.DBBook]:
     return db.query(models.DBBook).filter(
         models.DBBook.author_id == author_id
     ).all()
 
 
-def get_author_by_name(db: Session, name: str) -> Query:
+def get_author_by_name(db: Session, name: str) -> models.DBAuthor | None:
     return (
         db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
     )
