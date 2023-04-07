a9dab501a59b581e72eb8d31575821f547f3e6d4
Anna Kabatova <kabatova.anna@ukr.net>
2023-04-07 19:12:35 +0300

    Create models for data validation and serialization

---
+++ schemas.py
@@ -0,0 +1,43 @@
+from datetime import date
+from typing import List
+
+from pydantic import BaseModel
+
+from models import Book
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+    books: List[Book]
+
+    class Config:
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+    author_id: int
+
+
+class BookCreate(BookBase):
+    pass
+
+
+class Book(BookBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+        arbitrary_types_allowed = True
