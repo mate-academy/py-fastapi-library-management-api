84ee54ea9db857f9fa5416abd006dca4bffc9e38
Artem Kazakov <artemkazakov947@gmail.com>
2023-04-10 16:51:36 +0200

    Type annotations in crud.py and main.py were added

--- crud.py
+++ crud.py
@@ -4,7 +4,7 @@ import models
 import schemas
 
 
-def create_author(author: schemas.AuthorCreate, db: Session):
+def create_author(author: schemas.AuthorCreate, db: Session) -> models.Author:
     new_author = models.Author(
         name=author.name,
         bio=author.bio,
@@ -15,24 +15,24 @@ def create_author(author: schemas.AuthorCreate, db: Session):
     return new_author
 
 
-def get_author_list(db: Session):
-    return db.query(models.Author).all()
+def get_author_list(skip: int, limit: int, db: Session) -> list[models.Author]:
+    return db.query(models.Author).all()[skip:limit]
 
 
-def get_single_author(author_id: int, db: Session):
+def get_single_author(author_id: int, db: Session) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_book_list(db: Session, author_id: int | None = None):
+def get_book_list(skip: int, limit: int, db: Session, author_id: int | None = None) -> list[models.Book]:
     queryset = db.query(models.Book)
 
     if author_id is not None:
         queryset = queryset.filter(models.Book.author.has(id=author_id))
 
-    return queryset.all()
+    return queryset.all()[skip:limit]
 
 
-def create_book(db: Session, book: schemas.BookCreate, author_id: int):
+def create_book(db: Session, book: schemas.BookCreate, author_id: int) -> models.Book:
     new_book = models.Book(
         title=book.title,
         summary=book.summary,
--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def read_authors(
         db: Session = Depends(get_db),
         skip: int | None = None,
         limit: int | None = None,
-                 ):
-    return crud.get_author_list(db=db)[skip:limit]
+                 ) -> list[schemas.AuthorRead]:
+    return crud.get_author_list(db=db, skip=skip, limit=limit)
 
 
 @library.post("/author-create/", response_model=schemas.AuthorRead)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)) -> schemas.AuthorRead:
     return crud.create_author(author=author, db=db)
 
 
@@ -40,7 +40,7 @@ def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
 def get_author(
         author_id: int,
         db: Session = Depends(get_db),
-):
+) -> HTTPException | schemas.AuthorRead:
     author = crud.get_single_author(db=db, author_id=author_id)
 
     if author is None:
@@ -49,7 +49,7 @@ def get_author(
 
 
 @library.post("/authors/{author_id}/create-book/", response_model=schemas.BookList)
-def create_book(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
+def create_book(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)) -> schemas.BookList:
     return crud.create_book(db=db, book=book, author_id=author_id)
 
 
@@ -59,5 +59,5 @@ def get_books(
         author_id: int | None = None,
         skip: int | None = None,
         limit: int | None = None
-):
-    return crud.get_book_list(db=db, author_id=author_id)[skip:limit]
+) -> list[schemas.BookList]:
+    return crud.get_book_list(db=db, author_id=author_id, skip=skip, limit=limit)
