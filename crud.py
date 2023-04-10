73cb32b29cdfb646193e127a38c9e25101ebe63a
Vika <viktorykoroliova@gmail.com>
2023-04-10 15:00:21 +0300

    solution

---
+++ crud.py
@@ -0,0 +1,61 @@
+from sqlalchemy.orm import Session
+import schemas
+import models
+
+
+def get_author_by_name(db: Session, name: str) -> models.Author:
+    return db.query(models.Author).filter(models.Author.name == name).first()
+
+
+def get_authors_list(
+        db: Session,
+        offset: int,
+        limit: int
+) -> list[models.Author]:
+    return db.query(models.Author).offset(offset).limit(limit).all()
+
+
+def get_author(db: Session, author_id: int) -> models.Author:
+    return db.query(models.Author).filter(
+        models.Author.id == author_id
+    ).first()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
+    db_author = models.Author(
+        name=author.name,
+        bio=author.bio
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_books_list(
+        db: Session,
+        offset: int,
+        limit: int,
+        author_id: int | None = None
+) -> list[models.Book]:
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(models.Book.author_id == author_id)
+
+    return queryset.offset(offset).limit(limit).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
