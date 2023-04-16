5b2f73aa9f752a90f3e0ff95e4b0955d562e0098
AlenOl <stoveprofi@gmail.com>
2023-04-16 14:22:30 +0100

    Corrected type annotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import crud
+import models
 import schemas
 from database import SessionLocal
 
@@ -21,7 +22,7 @@ def root() -> dict:
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def read_authors(db: Session = Depends(get_db)):
+def read_authors(db: Session = Depends(get_db)) -> list[models.Author]:
     return crud.get_all_authors(db=db)
 
 
@@ -29,7 +30,7 @@ def read_authors(db: Session = Depends(get_db)):
 def create_author(
     author: schemas.AuthorCreate,
     db: Session = Depends(get_db),
-):
+) -> models.Author:
     db_author = crud.get_authors_by_name(db=db, name=author.name)
     if db_author:
         raise HTTPException(
@@ -40,7 +41,7 @@ def create_author(
 
 
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def read_single_author(author_id: int, db: Session = Depends(get_db)):
+def read_single_author(author_id: int, db: Session = Depends(get_db)) -> models.Author:
     db_author = crud.get_author(db=db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -48,7 +49,7 @@ def read_single_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.get("/books/", response_model=list[schemas.Book])
-def read_books(db: Session = Depends(get_db)):
+def read_books(db: Session = Depends(get_db)) -> list[models.Book]:
     return crud.get_all_books(db=db)
 
 
@@ -56,12 +57,12 @@ def read_books(db: Session = Depends(get_db)):
 def create_books(
     book: schemas.BookCreate,
     db: Session = Depends(get_db),
-):
+) -> models.Book:
     return crud.create_book(db=db, book=book)
 
 
 @app.get("/books/book_by_author_id/", response_model=schemas.Book)
-def read_single_book(author_id: int, db: Session = Depends(get_db)):
+def read_single_book(author_id: int, db: Session = Depends(get_db)) -> models.Book:
     db_book = crud.get_book(db=db, author_id=author_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book not found")
