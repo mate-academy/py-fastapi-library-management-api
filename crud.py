4e6db90c04b5e123d8b5a24ab1b7f2b0000bdc27
Bohdan2001007 <bogdan1991007@gmail.com>
2023-04-29 16:15:36 +0200

    Solution

--- crud.py
+++ crud.py
@@ -1,16 +1,17 @@
 from sqlalchemy.orm import Session
+from typing import List, Optional
 import models, schemas
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> Optional[models.Author]:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> Optional[models.Author]:
     return db.query(models.Author).filter(models.Author.name == name).first()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -18,19 +19,19 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_all_authors(db: Session, skip: int, limit: int):
+def get_all_authors(db: Session, skip: int, limit: int) -> List[models.Author]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def get_book_by_title(db: Session, title: str):
+def get_book_by_title(db: Session, title: str) -> Optional[models.Book]:
     return db.query(models.Book).filter(models.Book.title == title).first()
 
 
-def get_books_by_author(db: Session, author_id: int):
+def get_books_by_author(db: Session, author_id: int) -> List[models.Book]:
     return db.query(models.Book).filter(models.Book.author_id == author_id).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_author = get_author_by_name(db, book.author)
     if not db_author:
         db_author = create_author(db, schemas.AuthorCreate(name=book.author, bio=None))
@@ -41,5 +42,5 @@ def create_book(db: Session, book: schemas.BookCreate):
     return db_book
 
 
-def get_all_books(db: Session, skip: int, limit: int):
+def get_all_books(db: Session, skip: int, limit: int) -> List[models.Book]:
     return db.query(models.Book).offset(skip).limit(limit).all()
