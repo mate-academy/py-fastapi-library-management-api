455cbaf2077513206355431cc869650fd4a3ad9c
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-05-03 00:02:05 +0300

    Added "authors" endpoints

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+import models
+import schemas
+from database import engine, SessionLocal
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+models.Base.metadata.create_all(bind=engine)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/authors/", response_model=schemas.AuthorCreate)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    author_name = crud.get_autor_by_name(db=db, autor_name=author.name)
+    if author_name:
+        raise HTTPException(status_code=400, detail="This name already exists")
+    return crud.create_author(db=db, author=author)
+
+
+@app.get("/authors/", response_model=list[schemas.AuthorMain])
+def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    authors = crud.get_author_list(db=db, skip=skip, limit=limit)
+    return authors
