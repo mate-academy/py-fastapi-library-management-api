0ffe5ea754581fffb1c8b80a5923d70ad396226f
Roman Senyshyn <senjaskorp@gmail.com>
2023-05-02 16:30:53 +0300

    1 commit

---
+++ crud.py
@@ -0,0 +1,67 @@
+from sqlalchemy.orm import Session
+
+from db.models import Author, Book
+from schemas import (
+    AuthorCreate,
+    BookCreate,
+    BookCreateForAuthor
+)
+
+
+def get_all_authors_with_pagination(
+        db: Session,
+        skip: int = 0,
+        limit: int = 100
+):
+    return db.query(Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_id(db: Session, author_id: int):
+    return db.query(Author).filter(Author.id == author_id).first()
+
+
+def get_author_by_name(db: Session, author_name: str):
+    return db.query(Author).filter(
+        Author.name == author_name
+    ).first()
+
+
+def create_author(db: Session, author: AuthorCreate):
+    db_author = Author(**author.dict())
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def create_book_for_author(
+        db: Session,
+        book: BookCreateForAuthor,
+        author_id: int
+):
+    db_book = Book(**book.dict(), author_id=author_id)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def create_book(db: Session, book: BookCreate):
+    db_book = Book(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def get_all_books_with_pagination(
+        db: Session,
+        skip: int = 0,
+        limit: int = 100,
+        author_id=None
+):
+    if author_id:
+        return db.query(Book).filter(
+            Book.author_id == author_id
+        ).offset(skip).limit(limit).all()
+    return db.query(Book).offset(skip).limit(limit).all()
---
+++ db/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Date,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
+
+from db.engine import Base
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    bio = Column(String(255), nullable=False)
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(511), nullable=False)
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("authors.id"))
+    author = relationship("Author", back_populates="books")
