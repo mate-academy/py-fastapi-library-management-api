6b55a2573382db8aa892227753ae305b89578c97
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-05-03 00:01:31 +0300

    Added validator

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from models import Book, Author
 
@@ -29,7 +29,15 @@ class BookBase(BaseModel):
 
 
 class BookCreate(BookBase):
-    pass
+    author_ids: list[int] = []
+
+    @validator("author_ids")
+    def validate_author_ids(self, author_ids, *, values):
+        existing_author_ids = {author.id for author in values.get("authors", [])}
+        for author_id in author_ids:
+            if author_id not in existing_author_ids:
+                raise ValueError(f"Author with ID {author_id} doesn't exist")
+        return author_ids
 
 
 class BookMain(BookBase):
