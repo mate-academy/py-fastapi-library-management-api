f19a3c6f1fadaaafc1ebd3bc5c6171e93cd7c9f9
Maksym Nikityuk <maksym71417@gmail.com>
2023-05-04 00:44:51 +0300

    Solution

---
+++ crud.py
@@ -0,0 +1,48 @@
+from sqlalchemy.orm import Session
+from db import models
+import schemas
+
+
+def get_all_books(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Book).offset(skip).limit(limit).all()
+
+
+def get_book(db: Session, book_id: int):
+    return db.query(models.Book).filter(models.Book.id == book_id).first()
+
+
+def filter_books_by_author(db: Session, author_id: int):
+    return db.query(models.Book).filter(
+        models.Book.author_id == author_id).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
+
+
+def get_all_authors(db: Session, skip: int = 0, limit: int = 50):
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author(db: Session, author_id: int):
+    return db.query(models.Author).filter(models.Author.id == author_id).first()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Book(
+        name=author.name,
+        bio=author.bio,
+    )
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
