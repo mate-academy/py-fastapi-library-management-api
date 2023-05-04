0e45b7d28c9edb63f517c7eae5b2194ca0c04606
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-04 18:50:02 +0300

    Created CRUD utility functions for authors and books

---
+++ crud.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm import Session
+
+import schemas
+from db import models
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(**author.dict())
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_all_authors(db: Session, skip: int = 0, limit: int = 5):
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_name(db: Session, author_name: str):
+    return (
+        db.query(models.Author)
+        .filter(models.Author.name == author_name)
+        .first()
+    )
+
+
+def get_author_by_id(db: Session, author_id: int):
+    return (
+        db.query(models.Author).filter(models.Author.id == author_id).first()
+    )
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
+
+
+def get_all_books(
+    db: Session, author_id: int = None, skip: int = 0, limit: int = 5
+):
+    query = db.query(models.Book)
+
+    if author_id:
+        query = query.filter(models.Book.author_id == author_id)
+
+    return query.offset(skip).limit(limit).all()
