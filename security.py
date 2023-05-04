f597d33bf9d16e9411a03a752649462c063f2c90
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-05-04 21:49:19 +0200

    Added identation

--- security.py
+++ security.py
@@ -93,6 +93,7 @@ def get_current_active_user(
         current_user: schemas.User = Depends(get_current_user)
 ):
     if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user"
-                            )
+        raise HTTPException(
+            status_code=400, detail="Inactive user"
+        )
     return current_user
