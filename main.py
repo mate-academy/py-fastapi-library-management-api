1d481085b9b7d6c64e7cfb09902716255c91fa50
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-05-04 18:43:07 +0200

    Fix functions annotations

--- main.py
+++ main.py
@@ -17,20 +17,22 @@ def get_db() -> Session:
 
 
 @app.get("/")
-def root():
+def root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
 def read_authors(
     skip: int = 0, limit: int = 20, db: Session = Depends(get_db)
-):
+) -> list[schemas.Author]:
     authors = crud.get_all_authors(db=db)
     return authors[skip : skip + limit]
 
 
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def read_single_author(author_id: int, db: Session = Depends(get_db)):
+def read_single_author(
+    author_id: int, db: Session = Depends(get_db)
+) -> schemas.Author:
     db_author = crud.get_author(db=db, author_id=author_id)
 
     if db_author is None:
@@ -42,7 +44,9 @@ def read_single_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(
+    author: schemas.AuthorCreate, db: Session = Depends(get_db)
+) -> schemas.Author:
     db_author = crud.get_author_by_name(db=db, name=author.name)
 
     if db_author:
@@ -59,11 +63,13 @@ def read_books(
     limit: int = 20,
     author_id: int = None,
     db: Session = Depends(get_db),
-):
+) -> list[schemas.Book]:
     books = crud.get_books_list(db=db, author_id=author_id)
     return books[skip : skip + limit]
 
 
 @app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+def create_book(
+    book: schemas.BookCreate, db: Session = Depends(get_db)
+) -> schemas.Book:
     return crud.create_book(db=db, book=book)
