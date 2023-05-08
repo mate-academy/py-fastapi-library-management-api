658bb8efb733734e8cc00cc30988c15b8835d1cf
Oleksandr Leha <legus96@gmail.com>
2023-05-08 21:02:38 +0300

    Solution_added_endpoints

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 
-from database import engine, SessionLocal
 import crud
 import models
 import schemas
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+# Dependency to get database session
 def get_db():
     db = SessionLocal()
     try:
@@ -19,13 +21,16 @@ def get_db():
         db.close()
 
 
+# Author API endpoints
 @app.post("/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
-    db_author = crud.create_author(db, author)
-    return db_author
+    db_author = crud.get_author_by_name(db, name=author.name)
+    if db_author:
+        raise HTTPException(status_code=400, detail="Author with this name already exists")
+    return crud.create_author(db=db, author=author)
 
 
-@app.get("/authors/", response_model=list[schemas.Author])
+@app.get("/authors/", response_model=List[schemas.Author])
 def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     authors = crud.get_authors(db, skip=skip, limit=limit)
     return authors
@@ -39,12 +44,32 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
     return db_author
 
 
+@app.put("/authors/{author_id}", response_model=schemas.Author)
+def update_author(author_id: int, author: schemas.AuthorUpdate, db: Session = Depends(get_db)):
+    db_author = crud.update_author(db, author_id=author_id, author=author)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_author
+
+
+@app.delete("/authors/{author_id}", response_model=schemas.Author)
+def delete_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.delete_author(db, author_id=author_id)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_author
+
+
+# Book API endpoints
 @app.post("/authors/{author_id}/books/", response_model=schemas.Book)
 def create_book_for_author(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db, author_id=author_id)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
     return crud.create_book(db=db, book=book, author_id=author_id)
 
 
-@app.get("/books/", response_model=list[schemas.Book])
+@app.get("/books/", response_model=List[schemas.Book])
 def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     books = crud.get_books(db, skip=skip, limit=limit)
     return books
@@ -58,9 +83,17 @@ def read_book(book_id: int, db: Session = Depends(get_db)):
     return db_book
 
 
-@app.get("/authors/{author_id}/books/", response_model=list[schemas.Book])
-def read_books_for_author(author_id: int, db: Session = Depends(get_db)):
-    books = crud.get_books_for_author(db, author_id=author_id)
-    if books is None:
-        raise HTTPException(status_code=404, detail="Author not found")
-    return books
+@app.put("/books/{book_id}", response_model=schemas.Book)
+def update_book(book_id: int, book: schemas.BookUpdate, db: Session = Depends(get_db)):
+    db_book = crud.update_book(db, book_id=book_id, book=book)
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db_book
+
+
+@app.delete("/books/{book_id}", response_model=schemas.Book)
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = crud.delete_book(db, book_id=book_id)
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return db_book
