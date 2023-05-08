c489177691c63c2d655aa15ce3ef834b1c09b70f
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-08 14:43:19 +0300

    Add schemas with author chemas

---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class AuthorBase(BaseModel):
+    name:  str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
