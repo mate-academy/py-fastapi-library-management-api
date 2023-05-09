412cb443936293b17c088097c7da077b9a0b186c
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-05-09 16:21:39 +0300

    use black for crud

--- crud.py
+++ crud.py
@@ -6,19 +6,29 @@ from db import models
 import schemas
 
 
-def get_all_authors(db: Session, skip: int = 0, limit: int = 5) -> List[models.DBAuthor]:
+def get_all_authors(
+    db: Session, skip: int = 0, limit: int = 5
+) -> List[models.DBAuthor]:
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
 def get_author_by_name(db: Session, name: str) -> Optional[models.DBAuthor]:
-    return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+    return (
+        db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+    )
 
 
 def get_author(db: Session, author_id: int) -> Optional[models.DBAuthor]:
-    return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
+    return (
+        db.query(models.DBAuthor)
+        .filter(models.DBAuthor.id == author_id)
+        .first()
+    )
 
 
-def create_author(db: Session, author: schemas.AuthorCreate) -> models.DBAuthor:
+def create_author(
+    db: Session, author: schemas.AuthorCreate
+) -> models.DBAuthor:
     db_author = models.DBAuthor(
         name=author.name,
         bio=author.bio,
@@ -29,8 +39,14 @@ def create_author(db: Session, author: schemas.AuthorCreate) -> models.DBAuthor:
     return db_author
 
 
-def update_author(db: Session, author_id: int, author: schemas.Author) -> Optional[models.DBAuthor]:
-    db_author = db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
+def update_author(
+    db: Session, author_id: int, author: schemas.Author
+) -> Optional[models.DBAuthor]:
+    db_author = (
+        db.query(models.DBAuthor)
+        .filter(models.DBAuthor.id == author_id)
+        .first()
+    )
     if not db_author:
         return None
     for field, value in vars(author).items():
@@ -42,9 +58,11 @@ def update_author(db: Session, author_id: int, author: schemas.Author) -> Option
 
 
 def delete_author(db: Session, author_id: int) -> Optional[models.DBAuthor]:
-    db_author = db.query(models.DBAuthor).filter(
-        models.DBAuthor.id == author_id
-    ).first()
+    db_author = (
+        db.query(models.DBAuthor)
+        .filter(models.DBAuthor.id == author_id)
+        .first()
+    )
     if not db_author:
         return None
     db.delete(db_author)
@@ -53,10 +71,10 @@ def delete_author(db: Session, author_id: int) -> Optional[models.DBAuthor]:
 
 
 def get_book_list(
-        db: Session,
-        author: Optional[str] = None,
-        skip: int = 0,
-        limit: int = 5,
+    db: Session,
+    author: Optional[str] = None,
+    skip: int = 0,
+    limit: int = 5,
 ) -> List[models.DBBook]:
     queryset = db.query(models.DBBook).offset(skip).limit(limit)
 
@@ -75,7 +93,7 @@ def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
         title=book.title,
         summary=book.summary,
         publication_date=book.publication_date,
-        author_id=book.author_id
+        author_id=book.author_id,
     )
     db.add(db_book)
     db.commit()
@@ -83,8 +101,12 @@ def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
     return db_book
 
 
-def update_book(db: Session, book_id: int, book: schemas.BookBase) -> Optional[models.DBBook]:
-    db_book = db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
+def update_book(
+    db: Session, book_id: int, book: schemas.BookBase
+) -> Optional[models.DBBook]:
+    db_book = (
+        db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
+    )
     if not db_book:
         return None
     for field, value in vars(book).items():
@@ -96,7 +118,9 @@ def update_book(db: Session, book_id: int, book: schemas.BookBase) -> Optional[m
 
 
 def delete_book(db: Session, book_id: int) -> Optional[models.DBBook]:
-    db_book = db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
+    db_book = (
+        db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
+    )
     if not db_book:
         return None
     db.delete(db_book)
