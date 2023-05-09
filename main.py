30e6774d97eb04df7ea3d6ffc9751a462636bf7e
manjustice <manjustice1998@gmail.com>
2023-05-09 16:15:43 +0300

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import Response
 from sqlalchemy.orm import Session
 
 import crud
@@ -21,12 +22,12 @@ def read_authors(
         skip: int = 0,
         limit: int = 50,
         db: Session = Depends(get_db)
-):
+) -> Response:
     return crud.get_authors(db=db, skip=skip, limit=limit)
 
 
-@app.get("/authors/{author_id}", response_model=schemas.Author)
-def read_author(author_id: int, db: Session = Depends(get_db)):
+@app.get("/authors/{author_id}/", response_model=schemas.Author)
+def read_author(author_id: int, db: Session = Depends(get_db)) -> Response:
     author = crud.get_author(db=db, author_id=author_id)
 
     if author is None:
@@ -36,7 +37,10 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(
+        author: schemas.AuthorCreate,
+        db: Session = Depends(get_db)
+) -> Response:
     return crud.create_author(db=db, author=author)
 
 
@@ -45,15 +49,21 @@ def read_books(
         skip: int = 0,
         limit: int = 50,
         db: Session = Depends(get_db)
-):
+) -> Response:
     return crud.get_books(db=db, skip=skip, limit=limit)
 
 
 @app.get("/books/{author_id}/", response_model=list[schemas.Book])
-def read_book_by_author(author_id: int, db: Session = Depends(get_db)):
+def read_book_by_author(
+        author_id: int,
+        db: Session = Depends(get_db)
+) -> Response:
     return crud.get_books(db=db, author_id=author_id)
 
 
 @app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+def create_book(
+        book: schemas.BookCreate,
+        db: Session = Depends(get_db)
+) -> Response:
     return crud.create_book(db=db, book=book)
