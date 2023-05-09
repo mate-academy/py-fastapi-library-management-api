1267f77b5ce9ff80ce8a84c79861a81c899927a1
name <ksyusha0kubay@gmail.com>
2023-05-09 18:47:39 +0300

    Fix annotations

--- crud.py
+++ crud.py
@@ -4,15 +4,15 @@ from db import models
 import schemas
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_all_authors(db: Session):
+def get_all_authors(db: Session) -> list[models.Author]:
     return db.query(models.Author).all()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -20,18 +20,18 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books_by_author(db: Session, author_id: int):
+def get_books_by_author(db: Session, author_id: int) -> list[models.Book]:
     query = db.query(models.Book)
     if author_id:
         query = query.filter(models.Book.author_id == author_id)
     return query.all()
 
 
-def get_all_books(db: Session):
+def get_all_books(db: Session) -> list[models.Book]:
     return db.query(models.Book).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> listmodels.Book:
     db_book = models.Book(
         title=book.title,
         summary=book.summary,
