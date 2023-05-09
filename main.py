f7536622e2a355aac7a1d62106c0089caa2ab8c0
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-05-09 15:35:30 +0200

    Fixed identations

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+from typing import List
+
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -6,6 +8,7 @@ from starlette import status
 
 import crud
 import schemas
+from models import User, Author, Book
 from schemas import Token
 from security import (
     create_access_token,
@@ -28,11 +31,11 @@ def get_db() -> Session:
         db.close()
 
 
-@app.post("/token", response_model=Token)
+@app.post("/token/", response_model=Token)
 def login_for_access_token(
         db: Session = Depends(get_db),
         form_data: OAuth2PasswordRequestForm = Depends()
-):
+) -> dict:
     user = authenticate_user(
         db=db,
         username=form_data.username,
@@ -52,16 +55,19 @@ def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.post("/sign-up", response_model=schemas.User)
-async def create_user(user: schemas.User, db: Session = Depends(get_db), ):
+@app.post("/sign-up/", response_model=schemas.User)
+async def create_user(
+        user: schemas.User,
+        db: Session = Depends(get_db)
+) -> User:
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/me", response_model=schemas.User)
+@app.get("/users/me/", response_model=schemas.User)
 def read_users_me(
         token=Depends(oauth_2_scheme),
         db: Session = Depends(get_db)
-):
+) -> User:
     user = get_current_user(db=db, token=token)
     return user
 
@@ -70,12 +76,15 @@ def read_users_me(
 def create_author(
         author: schemas.AuthorCreate,
         db: Session = Depends(get_db),
-):
+) -> Author:
     return crud.create_author(db=db, author=author)
 
 
 @app.get("/author/{author_id}/", response_model=schemas.Author)
-def read_single_author(author_id: int, db: Session = Depends(get_db)):
+def read_single_author(
+        author_id: int,
+        db: Session = Depends(get_db)
+) -> Author:
     db_author = crud.get_author(db=db, author_id=author_id)
 
     if db_author is None:
@@ -85,11 +94,14 @@ def read_single_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.delete("/author/{author_id}/", status_code=204)
-def delete_single_author(author_id: int, db: Session = Depends(get_db)):
+def delete_single_author(
+        author_id: int,
+        db: Session = Depends(get_db)
+) -> None:
     author = crud.get_author(db=db, author_id=author_id)
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
-    return crud.delete_author(db=db, author_id=author_id)
+    crud.delete_author(db=db, author_id=author_id)
 
 
 @app.get("/books/", response_model=list[schemas.Book])
@@ -99,7 +111,7 @@ def read_books(
         skip=2, limit=5,
         title=None,
 
-):
+) -> List[Book]:
     return crud.get_book_list(
         db=db,
         skip=skip,
@@ -113,13 +125,16 @@ def read_books(
 def create_book(
         book: schemas.BookCreate,
         db: Session = Depends(get_db),
-):
+) -> Book:
     return crud.create_book(db=db, book=book)
 
 
 @app.delete("/book/{book_id}/", status_code=204)
-def delete_single_book(book_id: int, db: Session = Depends(get_db)):
+def delete_single_book(
+        book_id: int,
+        db: Session = Depends(get_db)
+) -> None:
     author = crud.get_book(db=db, book_id=book_id)
     if not author:
         raise HTTPException(status_code=404, detail="Author not found")
-    return crud.delete_author(db=db, author_id=book_id)
+    crud.delete_author(db=db, author_id=book_id)
