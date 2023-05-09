07617d93d860e6b6bda50e8db80b1b4963f8bbfa
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-05-10 00:42:06 +0300

    Solution

--- crud.py
+++ crud.py
@@ -4,19 +4,19 @@ import models
 import schemas
 
 
-def get_authors(db: Session):
+def get_authors(db: Session) -> list[models.Author]:
     return db.query(models.Author).all()
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> models.Author:
     return db.query(models.Author).filter(models.Author.name == name).first()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(name=author.name, bio=author.bio)
 
     db.add(db_author)
@@ -26,15 +26,15 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 100):
+def get_books(db: Session, skip: int = 0, limit: int = 100) -> list[models.Book]:
     return db.query(models.Book).offset(skip).limit(limit).all()
 
 
-def get_book(db: Session, book_id: int):
+def get_book(db: Session, book_id: int) -> models.Book:
     return db.query(models.Book).filter(models.Book.id == book_id).first()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(
         title=book.title,
         summary=book.summary,
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def read_authors(
     return crud.get_authors(db, skip=skip, limit=limit)
 
 
-@app.get("/authors/{author_id}", response_model=schemas.Author)
+@app.get("/authors/{author_id}/", response_model=schemas.Author)
 def read_single_author(author_id: int, db: Session = Depends(get_db)):
     author = crud.get_author(db=db, author_id=author_id)
 
