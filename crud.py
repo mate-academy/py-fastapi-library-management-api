f6c4e91a2b1bf6caf9c40a78a22c04d14e2d4d85
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-10 15:42:51 +0300

    Add return annotations to crud

--- crud.py
+++ crud.py
@@ -2,31 +2,32 @@ from sqlalchemy.orm import Session
 
 import models
 import schemas
+from models import Author
+from models import Book
 
 
-def get_all_authors(db: Session, skip: int = 0, limit: int = 10):
+def get_all_authors(db: Session, skip: int = 0, limit: int = 10) -> list[Author]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def create_author(db: Session, author: schemas.Author):
-    db_author = models.Author(
+def create_author(db: Session, author: schemas.Author) -> Author:
+    db_author = Author(
         name=author.name,
         bio=author.bio
     )
     db.add(db_author)
     db.commit()
     db.refresh(db_author)
-
     return db_author
 
 
-def get_detail_author(db: Session, author_id: int):
+def get_detail_author(db: Session, author_id: int) -> Author:
     return (
         db.query(models.Author).filter(models.Author.id == author_id).first()
     )
 
 
-def update_author(db: Session, author_id: int, author: schemas.AuthorUpdate):
+def update_author(db: Session, author_id: int, author: schemas.AuthorUpdate) -> Author | None:
     author_to_update = get_detail_author(db, author_id)
     if not author_to_update:
         return None
@@ -37,7 +38,7 @@ def update_author(db: Session, author_id: int, author: schemas.AuthorUpdate):
     return author_to_update
 
 
-def delete_author(db: Session, author_id: int):
+def delete_author(db: Session, author_id: int) -> Author | None:
     author_to_delete = get_detail_author(db, author_id)
     if not author_to_delete:
         return None
@@ -51,7 +52,7 @@ def get_all_books(
         author_id: int | None = None,
         skip: int = 0,
         limit: int = 10
-):
+) -> Book:
     queryset = db.query(models.Book)
 
     if author_id is not None:
@@ -62,7 +63,7 @@ def get_all_books(
     return queryset.offset(skip).limit(limit).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> Book:
     db_book = models.Book(
         title=book.title,
         summary=book.summary,
