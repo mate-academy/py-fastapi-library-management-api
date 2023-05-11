1da26145d5742c627500c05b6da55294eaa87201
Eugene <eugene.lot77@gmail.com>
2023-05-11 13:21:10 +0300

    Added annotations

--- crud.py
+++ crud.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
 
 import schemas
@@ -5,17 +7,17 @@ from db import models
 from schemas import AuthorCreate
 
 
-def get_all_authors(db: Session):
+def get_all_authors(db: Session) -> list[schemas.Author]:
     return db.query(models.DBAuthor).all()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> Optional[models.DBAuthor]:
     return (
         db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
     )
 
 
-def create_author(db: Session, author: AuthorCreate):
+def create_author(db: Session, author: AuthorCreate) -> schemas.Author:
     db_author = models.DBAuthor(
         name=author.name,
         bio=author.bio,
@@ -30,7 +32,7 @@ def create_author(db: Session, author: AuthorCreate):
 def get_book_list(
     db: Session,
     author: str | None = None,
-):
+) -> Optional[models.DBAuthor]:
     queryset = db.query(models.DBBook)
 
     if author:
@@ -39,11 +41,11 @@ def get_book_list(
     return queryset.all()
 
 
-def get_book(db: Session, book_id: int):
+def get_book(db: Session, book_id: int) -> models.DBBook:
     return db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
     db_book = models.DBBook(
         title=book.title,
         summary=book.summary,
