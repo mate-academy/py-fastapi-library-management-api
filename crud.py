681dc395bf7f4abf03093f9cdf6bb43b93fc921e
Kateryna Vasylieva <katevvasylyeva@gmail.com>
2023-06-04 16:41:51 +0300

    Add type hints for returning values in crud.py functions

--- crud.py
+++ crud.py
@@ -1,22 +1,26 @@
+from typing import Type
+
 from sqlalchemy.orm import Session
 
 import models
 import schemas
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 100):
+def get_authors(
+    db: Session, skip: int = 0, limit: int = 100
+) -> list[Type[models.DBAuthor]]:
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> models.DBAuthor:
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> models.DBAuthor:
     return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.DBAuthor:
     db_author = models.DBAuthor(**author.dict())
     db.add(db_author)
     db.commit()
@@ -27,7 +31,7 @@ def create_author(db: Session, author: schemas.AuthorCreate):
 
 def get_books(
     db: Session, skip: int = 0, limit: int = 100, author_id: int | None = None
-):
+) -> list[Type[models.DBBook]]:
     queryset = db.query(models.DBBook)
     if author_id:
         queryset = queryset.filter(models.DBBook.author_id == author_id)
@@ -35,11 +39,11 @@ def get_books(
     return queryset.all()
 
 
-def get_book(db: Session, book_id: int):
+def get_book(db: Session, book_id: int) -> models.DBBook:
     return db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.DBBook:
     db_book = models.DBBook(**book.dict())
     db.add(db_book)
     db.commit()
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 @app.get("/authors/{author_id}", response_model=schemas.Author)
 def read_author(author_id: int, db: Session = Depends(get_db)):
     db_author = crud.get_author(db, author_id=author_id)
-    if db_author is None:
+    if not db_author:
         raise HTTPException(status_code=404, detail="Author not found")
     return db_author
 
@@ -60,6 +60,6 @@ def read_books(
 @app.get("/books/{book_id}", response_model=schemas.Book)
 def read_book(book_id: int, db: Session = Depends(get_db)):
     db_book = crud.get_book(db, book_id=book_id)
-    if db_book is None:
+    if not db_book:
         raise HTTPException(status_code=404, detail="Book not found")
     return db_book
