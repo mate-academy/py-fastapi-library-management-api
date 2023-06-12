a46ad18188895dad06520ed85f560daca11f1cbf
andriy-demeshko <ande777complaint@gmail.com>
2023-06-12 16:04:15 +0300

    Create SQLAlchemy models for 'Author' and 'Book' in the models.py file

---
+++ db/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class DBAuthor(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    bio = Column(String(500))
+    books = relationship("DBBook", backref="author")
+
+
+class DBBook(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(500))
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("authors.id"))
+
+    author = relationship("DBAuthor")
