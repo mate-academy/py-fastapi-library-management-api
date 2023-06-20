8ff68869a2a443e9f0578b46354ccfcf8cd4f741
Dmytro Hushchyk <d.hushchyk@gmail.com>
2023-06-20 12:28:37 +0300

    created schemas.py

---
+++ schemas.py
@@ -0,0 +1,45 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author: Author
+
+    class Config:
+        orm_mode = True
+
+
+class AuthorBook(AuthorBase):
+    id: int
+    books: list[Book] = []
+
+    class Config:
+        orm_mode = True
