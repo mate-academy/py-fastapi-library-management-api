f6887709351b110cb828cdb209071d39ff49d6e7
Derkach Oleksandr <derkach03553@gmail.com>
2023-06-27 12:18:48 +0300

    Solution

--- crud.py
+++ crud.py
@@ -4,21 +4,21 @@ import models
 import schemas
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 100):
+def get_authors(db: Session, skip: int = 0, limit: int = 100) -> list[models.Author]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> models.Author:
     return (
         db.query(models.Author).filter(models.Author.name == name).first()
     )
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(
         name=author.name,
         bio=author.bio,
@@ -29,14 +29,14 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, author_id: int = None, skip: int = 0, limit: int = 100):
+def get_books(db: Session, author_id: int = None, skip: int = 0, limit: int = 100) -> list[models.Book]:
     queryset = db.query(models.Book)
     if author_id:
         queryset = queryset.filter(models.Book.author_id == author_id)
     return queryset.offset(skip).limit(limit).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(
         title=book.title,
         bio=book.bio,
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+import models
 import schemas
-from database import SessionLocal
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
