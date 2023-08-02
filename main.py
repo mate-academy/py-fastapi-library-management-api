68c78e36a25e55c2db38f67c7dd71a253d2face5
Mkrtych Khachatrian <mukuchbrat@gmail.com>
2023-08-02 21:28:50 +0300

    Solution

---
+++ main.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/authors/", response_model=List[schemas.AuthorList])
+def read_authors(
+        skip: int = 0,
+        limit: int = 100,
+        db: Session = Depends(get_db)
+):
+    authors = crud.get_authors(db, skip=skip, limit=limit)
+    return authors
+
+
+@app.get("/authors/{author_id}", response_model=schemas.AuthorList)
+def read_author(author_id: int, db: Session = Depends(get_db)):
+    author = crud.get_author(db, author_id=author_id)
+    if author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return author
+
+
+@app.post("/authors/", response_model=schemas.AuthorList)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_name(db, name=author.name)
+    if db_author:
+        raise HTTPException(status_code=400, detail="Author already exist")
+    return crud.create_author(db=db, author=author)
+
+
+@app.get("/books/", response_model=List[schemas.BookList])
+def read_books(
+        author_id: int | None = None,
+        skip: int = 0,
+        limit: int = 100,
+        db: Session = Depends(get_db)
+):
+    books = crud.get_books(db, author_id=author_id, skip=skip, limit=limit)
+    return books
+
+
+@app.post("/books/", response_model=schemas.BookCreate)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
