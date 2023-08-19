fd30978f394002adf1457fcd143a4a0913b83f3e
Glieb <gleblitvinenko3007@gmail.com>
2023-08-19 13:46:39 +0300

    FIXED SOLUTION

--- crud.py
+++ crud.py
@@ -6,7 +6,7 @@ import schemas
 # ⬇️ --- AUTHORS CRUD --- ⬇️
 
 
-def create_author(db: Session, author: schemas.AuthorCreateUpdate):
+def create_author(db: Session, author: schemas.AuthorCreateUpdate) -> models.Author:
     db_author = models.Author(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -14,15 +14,15 @@ def create_author(db: Session, author: schemas.AuthorCreateUpdate):
     return db_author
 
 
-def get_authors(db: Session, page: int = 0, page_size: int = 100):
+def get_authors(db: Session, page: int = 0, page_size: int = 100) -> list[models.Author]:
     return db.query(models.Author).offset(page).limit(page_size).all()
 
 
-def get_detailed_author(db: Session, author_id: int):
+def get_detailed_author(db: Session, author_id: int) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_author_by_name(db: Session, author_name: str):
+def get_author_by_name(db: Session, author_name: str) -> models.Author:
     return db.query(models.Author).filter(models.Author.name == author_name).first()
 
 
@@ -30,7 +30,7 @@ def update_author(
     db: Session,
     author_id_to_update: int,
     author_update_data: schemas.AuthorCreateUpdate,
-):
+) -> models.Author | None:
     db_author = get_detailed_author(db=db, author_id=author_id_to_update)
     if db_author is not None:
         for key, value in author_update_data.model_dump().items():
@@ -38,11 +38,10 @@ def update_author(
         db.commit()
         db.refresh(db_author)
         return db_author
-    else:
-        return None
+    return None
 
 
-def delete_author(db: Session, author_id_to_delete: int):
+def delete_author(db: Session, author_id_to_delete: int) -> models.Author | None:
     db_author = get_detailed_author(db=db, author_id=author_id_to_delete)
     if db_author is not None:
         db.delete(db_author)
@@ -54,7 +53,7 @@ def delete_author(db: Session, author_id_to_delete: int):
 # ⬇️ --- BOOKS CRUD --- ⬇️
 
 
-def create_book(db: Session, book_to_create: schemas.BookCreateUpdate):
+def create_book(db: Session, book_to_create: schemas.BookCreateUpdate) -> models.Book:
     db_book = models.Book(**book_to_create.model_dump())
     db.add(db_book)
     db.commit()
@@ -62,17 +61,17 @@ def create_book(db: Session, book_to_create: schemas.BookCreateUpdate):
     return db_book
 
 
-def get_books(db: Session, page: int = 0, page_size: int = 100):
+def get_books(db: Session, page: int = 0, page_size: int = 100) -> list[models.Book]:
     return db.query(models.Book).offset(page).limit(page_size).all()
 
 
-def get_detailed_book(db: Session, book_id: int):
+def get_detailed_book(db: Session, book_id: int) -> models.Book:
     return db.query(models.Book).filter(models.Book.id == book_id).first()
 
 
 def update_book(
     db: Session, book_id_to_update: int, book_update_data: schemas.BookCreateUpdate
-):
+) -> models.Book | None:
     db_book = get_detailed_book(db=db, book_id=book_id_to_update)
 
     if db_book is not None:
@@ -84,14 +83,13 @@ def update_book(
     return None
 
 
-def get_book_by_title(db: Session, book_title: str):
+def get_book_by_title(db: Session, book_title: str) -> models.Book:
     return db.query(models.Book).filter(models.Book.title == book_title).first()
 
 
-def delete_book(db: Session, book_id: int):
+def delete_book(db: Session, book_id: int) -> models.Book | None:
     db_book = get_detailed_book(db=db, book_id=book_id)
     if db_book is not None:
         db.delete(db_book)
         db.commit()
-        return db_book
-    return None
+    return db_book
