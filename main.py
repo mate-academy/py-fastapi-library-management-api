c9249c4dad8942ceff70b8cef3373fe7186a2bca
Oleksandr Mamchur <irex1988@gmail.com>
2023-08-23 20:41:54 +0300

    added list endpoint for authors

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+import models
+import schemas
+from database import SessionLocal
 
 app = FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "hiu"}
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/authors/", response_model=list[schemas.Author])
+def list_authors(db: Session = Depends(get_db)):
+    return crud.get_all_authors(db=db)
+
+
+
