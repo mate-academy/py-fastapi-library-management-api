120bea00f296c20c0899e4e8b86f37d75da54e4d
Alex Yatsun <starcrafter4444@gmail.com>
2023-08-23 21:38:10 +0300

    Solution

---
+++ db/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from  db.engine import Base
+
+
+class DBAuthor(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    bio = Column(String(511), nullable=False)
+
+    books = relationship("DBBook")
+
+
+class DBBook(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(511), nullable=False)
+    publication_date = Column(Date, nullable=False)
+    author_id = Column(Integer, ForeignKey("author.id"), nullable=False)
+
+    author = relationship(DBAuthor)
+
