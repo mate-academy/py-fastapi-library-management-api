dcc9663211c9541bbae157daa991cd4e45ccd207
Oleksandr Mamchur <irex1988@gmail.com>
2023-08-23 20:42:54 +0300

    added create endpoint for authors

--- crud.py
+++ crud.py
@@ -8,6 +8,10 @@ def get_all_authors(db: Session):
     return db.query(models.Author).all()
 
 
+def get_author_by_name(db: Session, name: str):
+    return db.query(models.Author).filter(models.Author.name == name).first()
+
+
 def create_author(db: Session, author: AuthorCreate):
     db_author = models.Author(name=author.name, bio=author.bio)
     db.add(db_author)
--- main.py
+++ main.py
@@ -22,4 +22,11 @@ def list_authors(db: Session = Depends(get_db)):
     return crud.get_all_authors(db=db)
 
 
-
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_name(db=db, name=author.name)
+    if db_author:
+        raise HTTPException(
+            status_code=400, detail="This author is already exist in DB"
+        )
+    return crud.create_author(db=db, author=author)
