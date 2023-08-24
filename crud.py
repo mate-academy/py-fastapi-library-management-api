eb3010be887e7e1a8ef1788b3076923d885c048a
Oleksandr Mamchur <irex1988@gmail.com>
2023-08-24 16:00:07 +0300

    Solution

--- crud.py
+++ crud.py
@@ -1,11 +1,17 @@
 from sqlalchemy.orm import Session
 
 import models
-from schemas import AuthorCreate
+from schemas import AuthorCreate, BookCreate
 
 
-def get_all_authors(db: Session):
-    return db.query(models.Author).all()
+def get_all_authors(db: Session, skip: int, limit: int):
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author_by_id(db: Session, author_id: int):
+    return (
+        db.query(models.Author).filter(models.Author.id == author_id).first()
+    )
 
 
 def get_author_by_name(db: Session, name: str):
@@ -19,3 +25,29 @@ def create_author(db: Session, author: AuthorCreate):
     db.refresh(db_author)
 
     return db_author
+
+
+def get_all_books(db: Session, skip: int, limit: int, author_id: int = None):
+    queryset = db.query(models.Book)
+    if author_id is not None:
+        queryset = queryset.filter(models.Book.author_id == author_id)
+
+    return queryset.offset(skip).limit(limit).all()
+
+
+def get_book_by_title(db: Session, title: str):
+    return db.query(models.Book).filter(models.Book.title == title).first()
+
+
+def create_book(db: Session, book: BookCreate):
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def get_db() -> Session:
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def list_authors(db: Session = Depends(get_db)):
-    return crud.get_all_authors(db=db)
+def list_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_all_authors(skip=skip, limit=limit, db=db)
 
 
 @app.post("/authors/", response_model=schemas.Author)
@@ -30,3 +30,28 @@ def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
             status_code=400, detail="This author is already exist in DB"
         )
     return crud.create_author(db=db, author=author)
+
+
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def retrieve_author_by_id(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_id(db=db, author_id=author_id)
+    if db_author:
+        return db_author
+    raise HTTPException(
+        status_code=404, detail="Author not found"
+    )
+
+
+@app.get("/books/", response_model=list[schemas.Book])
+def list_books(author_id: int = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_all_books(skip=skip, limit=limit, db=db, author_id=author_id)
+
+
+@app.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_book = crud.get_book_by_title(db=db, title=book.title)
+    if db_book:
+        raise HTTPException(
+            status_code=400, detail="This book is already exist in DB"
+        )
+    return crud.create_book(db=db, book=book)
