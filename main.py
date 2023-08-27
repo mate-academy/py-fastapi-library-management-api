c37b95a1f85575567eb4074376b3f6d1c12becb2
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-27 15:11:31 +0200

    fixed

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+import models
 import schemas
-from database import SessionLocal
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
