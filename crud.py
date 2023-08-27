1019e64816531fe14c5517a02959a5d0d6d07b45
Tetiana Kozakova <kleotan901@gmail.com>
2023-08-27 18:09:54 +0200

    fixed

--- crud.py
+++ crud.py
@@ -1,22 +1,32 @@
+from typing import Type
+
 from sqlalchemy.orm import Session
 
 import models
 import schemas
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 100):
+def get_authors(
+        db: Session, skip: int = 0, limit: int = 100
+) -> list[Type[models.DBAuthor]]:
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(
+        db: Session, name: str
+) -> models.DBAuthor:
     return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
 
 
-def get_author(db: Session, author_id: int):
+def get_author(
+        db: Session, author_id: int
+) -> models.DBAuthor:
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(
+        db: Session, author: schemas.AuthorCreate
+) -> models.DBAuthor:
     db_author = models.DBAuthor(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -24,23 +34,27 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 100):
+def get_books(
+        db: Session, skip: int = 0, limit: int = 100
+) -> list[Type[models.DBBook]]:
     return db.query(models.DBBook).offset(skip).limit(limit).all()
 
 
-def get_book_by_title(db: Session, title: str):
+def get_book_by_title(db: Session, title: str) -> models.DBBook:
     return db.query(models.DBBook).filter(models.DBBook.title == title).first()
 
 
-def get_book_by_author_id(db: Session, author_id: int):
+def get_book_by_author_id(db: Session, author_id: int) -> models.DBBook:
     return db.query(models.DBBook).filter(models.DBBook.author_id == author_id)
 
 
-def get_book(db: Session, book_id: int):
+def get_book(db: Session, book_id: int) -> models.DBBook:
     return db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(
+        db: Session, book: schemas.BookCreate
+) -> models.DBBook:
     db_book = models.DBBook(
         title=book.title,
         summary=book.summary,
--- main.py
+++ main.py
@@ -19,24 +19,28 @@ def get_db():
         db.close()
 
 
-@app.get("/authors/", response_model=list[schemas.Author])
+@app.get("/authors/")
 def read_authors(
         skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
+) -> list[schemas.Author]:
     authors = crud.get_authors(skip=skip, limit=limit, db=db)
     return authors
 
 
-@app.get("/authors/{author_id}", response_model=schemas.Author)
-def read_author(author_id: int, db: Session = Depends(get_db)):
+@app.get("/authors/{author_id}")
+def read_author(
+        author_id: int, db: Session = Depends(get_db)
+) -> schemas.Author:
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
     return db_author
 
 
-@app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+@app.post("/authors/")
+def create_author(
+        author: schemas.AuthorCreate, db: Session = Depends(get_db)
+) -> schemas.Author:
     db_author = crud.get_author_by_name(db=db, name=author.name)
     if db_author:
         raise HTTPException(
@@ -47,24 +51,28 @@ def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db=db, author=author)
 
 
-@app.get("/books/", response_model=list[schemas.Book])
+@app.get("/books/")
 def read_books(
         skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
-):
+) -> list[schemas.Book]:
     books = crud.get_books(skip=skip, limit=limit, db=db)
     return books
 
 
-@app.get("/books/{book_id}", response_model=schemas.Book)
-def get_book(book_id: int, db: Session = Depends(get_db)):
+@app.get("/books/{book_id}")
+def get_book(
+        book_id: int, db: Session = Depends(get_db)
+) -> schemas.Book:
     db_book = crud.get_book(db, book_id=book_id)
     if db_book is None:
         raise HTTPException(status_code=404, detail="Book not found")
     return db_book
 
 
-@app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+@app.post("/books/")
+def create_book(
+        book: schemas.BookCreate, db: Session = Depends(get_db)
+) -> schemas.Book:
     db_book = crud.get_book_by_title(db=db, title=book.title)
     if db_book:
         raise HTTPException(
@@ -75,8 +83,8 @@ def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db=db, book=book)
 
 
-@app.get("/books/{author_id}/", response_model=list[schemas.Book])
+@app.get("/books/{author_id}/")
 def get_books_by_author_id(
         author_id: int, db: Session = Depends(get_db)
-):
+) -> list[schemas.Book]:
     return crud.get_book_by_author_id(db=db, author_id=author_id)
