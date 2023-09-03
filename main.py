e640dbc8920e5cae0cafaa0fbae9f09c6aeac1da
Rostyslav <rostyslav.furman@gmail.com>
2023-09-03 21:48:27 +0300

    my_commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from database import SessionLocal
+
+app = FastAPI()
+
+
+def get_data_base() -> Session:
+    data_base = SessionLocal()
+
+    try:
+        yield data_base
+    finally:
+        data_base.close()
+
+
+@app.get("/authors/", response_model=list[schemas.Author])
+def read_authors(
+        data_base: Session = Depends(get_data_base),
+        page: int = Query(ge=0, default=0),
+        size: int = Query(ge=1, le=100)
+) -> list:
+    return crud.get_all_authors(data_base, page, size)
+
+
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(
+    author: schemas.AuthorCreate,
+    data_base: Session = Depends(get_data_base)
+):
+    data_base_author = crud.get_author_by_name(data_base=data_base, name=author.name)
+
+    if data_base_author:
+        raise HTTPException(
+            status_code=400,
+            detail="Name already exist"
+        )
+
+    return crud.create_author(data_base=data_base, author=author)
+
+
+@app.get("/authors/{author_id}/", response_model=schemas.Author)
+def read_single_author(author_id: int, data_base: Session = Depends(get_data_base)):
+    data_base_author = crud.get_author(data_base=data_base, author_id=author_id)
+
+    if data_base_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+
+    return data_base_author
+
+
+@app.get("/books/", response_model=list[schemas.Book])
+def get_all_books(
+    data_base: Session = Depends(get_data_base),
+    author_id: int | None = None,
+    page: int = Query(ge=0, default=0),
+    size: int = Query(ge=1, le=100)
+):
+    return crud.get_all_books(
+        data_base=data_base, author_id=author_id, page=page, size=size
+    )
+
+
+@app.get("/books/book_id/", response_model=schemas.Book)
+def read_single_book(book_id: int, data_base: Session = Depends(get_data_base)):
+    data_base_book = crud.get_book(data_base=data_base, book_id=book_id)
+
+    if data_base_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return data_base_book
+
+
+@app.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, data_base: Session = Depends(get_data_base)):
+    return crud.create_book(data_base=data_base, book=book)
---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, nullable=False)
+    bio = Column(String)
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255))
+    summary = Column(String)
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("authors.id"))
+    author = relationship("Author", back_populates="books")
