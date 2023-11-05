58e683213ffffa3fa5fbeb0248dc573e2477fb82
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-11-05 03:41:51 +0200

    Extended functionality of views

--- crud.py
+++ crud.py
@@ -4,14 +4,22 @@ import models
 import schemas
 
 
-def get_all_authors(db: Session):
-    return db.query(models.Author).all()
+def get_all_authors(db: Session, offset: int = 0, limit: int = 10):
+    return db.query(models.Author).offset(offset).limit(limit).all()
 
 
-def get_author_by_name(db: Session, name: str) -> None | models.Author:
+def get_author_by_name(db: Session, name: str):
     return (
         db.query(models.Author)
         .filter(models.Author.name == name)
+        .first()
+    )
+
+
+def get_author_by_id(db: Session, author_id: int):
+    return (
+        db.query(models.Author)
+        .filter(models.Author.id == author_id)
         .first()
     )
 
@@ -25,8 +33,20 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_all_books(db: Session):
-    return db.query(models.Book).all()
+def get_book_list(
+    db: Session,
+    offset: int = 0,
+    limit: int = 10,
+    author_id: int = None,
+):
+    if author_id is not None:
+        return (
+            db.query(models.Book)
+            .filter(models.Book.author_id == author_id)
+            .offset(offset).limit(limit).all()
+        )
+
+    return db.query(models.Book).offset(offset).limit(limit).all()
 
 
 def create_book(db: Session, book: schemas.BookCreate):
--- main.py
+++ main.py
@@ -23,8 +23,13 @@ def get_db() -> Session:
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def read_authors(db: Session = Depends(get_db)):
-    return crud.get_all_authors(db=db)
+def read_authors(
+        limit: int = 10,
+        offset: int = 0,
+        db: Session = Depends(get_db)
+):
+
+    return crud.get_all_authors(db=db, limit=limit, offset=offset)
 
 
 @app.post("/authors/", response_model=schemas.Author)
@@ -43,9 +48,32 @@ def create_author(
     return crud.create_author(db=db, author=author)
 
 
+@app.get("/authors/{author_id}/", response_model=schemas.Author)
+def retrieve_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_id(db=db, author_id=author_id)
+
+    if db_author is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Author does not exist"
+        )
+
+    return db_author
+
+
 @app.get("/books/", response_model=list[schemas.Book])
-def read_books(db: Session = Depends(get_db)):
-    return crud.get_all_books(db=db)
+def read_books(
+    limit: int = 10,
+    offset: int = 0,
+    author_id: int | None = None,
+    db: Session = Depends(get_db)
+):
+    return crud.get_book_list(
+        db=db,
+        author_id=author_id,
+        limit=limit,
+        offset=offset
+    )
 
 
 @app.post("/books/", response_model=schemas.Book)
