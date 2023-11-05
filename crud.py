8751b06b1b47464dfe8d2b7e2d040d3587bbbea3
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-11-05 02:25:03 +0200

    Created CRUD utility functions for authors and books

---
+++ crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+
+
+def get_all_authors(db: Session):
+    return db.query(models.Author).all()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(name=author.name, bio=author.bio)
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+
+    return db_author
+
+
+def get_all_books(db: Session):
+    return db.query(models.Book).all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
