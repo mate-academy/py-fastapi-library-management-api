3aa98e2a20c4787678fcb1003eef161773e24895
Svitlana Savikhina <savihina111@gmail.com>
2023-11-06 21:53:55 +0200

    Corrected_solution

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def root():
 def read_authors(
         skip: int = Query(default=0),
         limit: int = Query(default=10),
-        db: Session = Depends(get_db)):
+        db: Session = Depends(get_db)) -> list[schemas.Author]:
     authors = crud.get_all_authors(db=db, skip=skip, limit=limit)
     return authors
 
@@ -35,7 +35,7 @@ def read_authors(
 def create_author(
         author: schemas.AuthorCreate,
         db: Session = Depends(get_db)
-):
+) -> schemas.Author:
     db_author = crud.get_author_by_name(db=db, name=author.name)
 
     if db_author:
@@ -47,7 +47,7 @@ def create_author(
 
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
-def get_single_author(author_id: int, db: Session = Depends(get_db)):
+def get_single_author(author_id: int, db: Session = Depends(get_db)) -> schemas.Author:
     db_author = crud.get_author(db, author_id)
     if db_author is None:
         raise HTTPException(status_code=400, detail="Author not found")
@@ -58,7 +58,7 @@ def get_single_author(author_id: int, db: Session = Depends(get_db)):
 def read_books(
         skip: int = Query(default=0),
         limit: int = Query(default=10),
-        db: Session = Depends(get_db)):
+        db: Session = Depends(get_db)) -> list[schemas.Book]:
     books = crud.get_all_books(db=db, skip=skip, limit=limit)
     return books
 
@@ -67,12 +67,11 @@ def read_books(
 def create_book(
         book: schemas.BookCreate,
         db: Session = Depends(get_db)
-):
+) -> schemas.Book:
     return crud.create_book(db=db, book=book)
 
 
 @app.get("/books/author/{author_id}/", response_model=schemas.Book)
-def get_books_by_author(author_id: int, db: Session = Depends(get_db)):
+def get_books_by_author(author_id: int, db: Session = Depends(get_db)) -> schemas.Book:
     db_book = crud.get_books_by_author_id(db=db, author_id=author_id)
     return db_book
-
