6608c439ec5bfb1b55ec65fc012ffed764cb9fef
Illia <kononenko2109@gmail.com>
2023-11-10 00:21:53 +0200

    Solution

--- schemas.py
+++ schemas.py
@@ -16,8 +16,9 @@ class BookCreate(BookBase):
 class Book(BookBase):
     id: int
 
-    class Config:
-        orm_mode = True
+    @classmethod
+    def from_orm(cls, obj):
+        return cls.from_attributes(**obj.__dict__)
 
 
 class AuthorBase(BaseModel):
@@ -29,9 +30,17 @@ class AuthorCreate(AuthorBase):
     pass
 
 
+from pydantic import BaseModel
+
+
 class Author(AuthorBase):
     id: int
     books: list[Book] = []
 
-    class Config:
-        orm_mode = True
+    @classmethod
+    def from_orm(cls, obj):
+        author_dict = obj.__dict__
+        author_dict["books"] = [
+            Book.from_orm(book) for book in author_dict.get("books", [])
+        ]
+        return cls.from_attributes(**author_dict)
