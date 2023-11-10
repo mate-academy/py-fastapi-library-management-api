e7f97f01e9ffadb8a5b6b56a9fed652e32a54479
Illia <kononenko2109@gmail.com>
2023-11-10 14:52:40 +0200

    renamed orm_mode → from_attributes

--- schemas.py
+++ schemas.py
@@ -17,8 +17,8 @@ class Book(BookBase):
     id: int
 
     @classmethod
-    def from_orm(cls, obj):
-        return cls.from_attributes(**obj.__dict__)
+    def from_attributes(cls, obj):
+        return cls(**obj.__dict__)
 
 
 class AuthorBase(BaseModel):
@@ -30,17 +30,14 @@ class AuthorCreate(AuthorBase):
     pass
 
 
-from pydantic import BaseModel
-
-
 class Author(AuthorBase):
     id: int
     books: list[Book] = []
 
     @classmethod
-    def from_orm(cls, obj):
+    def from_attributes(cls, obj):
         author_dict = obj.__dict__
         author_dict["books"] = [
-            Book.from_orm(book) for book in author_dict.get("books", [])
+            Book.from_attributes(book) for book in author_dict.get("books", [])
         ]
-        return cls.from_attributes(**author_dict)
+        return cls(**author_dict)
