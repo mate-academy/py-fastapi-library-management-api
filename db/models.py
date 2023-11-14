eac2cb04acf9f25d146d8a7f7d65836f17550d54
Andy <andy77andy@ukr.net>
2023-11-14 12:58:16 +0200

    Created models

---
+++ db/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, String, Date, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from db.engine import Base
+
+
+class DBBook(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255))
+    summary = Column(String(255), nullable=True)
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("author.id"))
+
+    author = relationship("DBAuthor")
+
+
+class DBAuthor(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True)
+    bio = Column(String(255), nullable=True)
+    books = relationship(DBBook,  back_populates="author")
