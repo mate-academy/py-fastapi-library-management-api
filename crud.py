75a45de7c28a0ecb8999279abf58576594403dee
Oleksandr <lotrikys@gmail.com>
2023-11-23 16:31:19 +0200

    Fix type annotaions

--- crud.py
+++ crud.py
@@ -1,16 +1,14 @@
-from typing import Any
-
 from sqlalchemy.orm import Session, Query
 
 from db import models
 import schemas
 
 
-def get_all_authors(db: Session) -> Query[Any]:
+def get_all_authors(db: Session) -> Query[models.DBAuthor]:
     return db.query(models.DBAuthor).all()
 
 
-def get_author_by_id(db: Session, author_id: int) -> Query[Any]:
+def get_author_by_id(db: Session, author_id: int) -> Query[models.DBAuthor]:
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
 
 
@@ -22,11 +20,11 @@ def create_author(db: Session, author: schemas.AuthorCreate) -> models.DBAuthor:
     return db_author
 
 
-def get_all_books(db: Session) -> Query[Any]:
+def get_all_books(db: Session) -> Query[models.DBBook]:
     return db.query(models.DBBook).all()
 
 
-def get_books_by_author_id(db: Session, author_id: int) -> Query[Any]:
+def get_books_by_author_id(db: Session, author_id: int) -> Query[models.DBBook]:
     return db.query(models.DBBook).filter(models.DBBook.author_id == author_id)
 
 
