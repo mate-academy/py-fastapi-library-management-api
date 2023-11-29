6fbf27ff9024a8f58b8562152e151919be10a9e0
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-11-29 20:09:25 +0200

    Create CRUD functions

---
+++ crud.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+
+import models, schemas
+
+
+def list_authors(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Author).offset(skip).limit(limit).all()
+
+
+def get_author(db: Session, author_id: int):
+    return db.query(models.Author).filter(models.Author.id == author_id).first()
+
+
+def get_author_by_name(db: Session, name: str):
+    return db.query(models.Author).filter(models.Author.name == name).first()
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = models.Author(**author.model_dump())
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def list_books(
+    db: Session,
+    skip: int = 0,
+    limit: int = 100,
+    author_id: int | None = None
+):
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(
+            models.Book.author_id == author_id
+        )
+
+    return queryset.offset(skip).limit(limit).all()
+
+
+def get_book(db: Session, book_id: int):
+    return db.query(models.Book).filter(models.Book.id == book_id).first()
+
+
+def create_book_for_author(
+    db: Session,
+    book: schemas.BookCreate,
+    author_id: int,
+):
+    db_book = models.Book(**book.model_dump(), author_id=author_id)
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
