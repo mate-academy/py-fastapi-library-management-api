4fb294d8bfba2bed27026b59cc8af148b64e78f4
Volodymyr Panov <vpanov25@gmail.com>
2023-12-03 16:33:01 +0100

    added return type annotations

--- crud.py
+++ crud.py
@@ -1,23 +1,24 @@
 from sqlalchemy.orm import Session
 import models
 import schemas
+from typing import Optional
 
 
-def get_all_authors(db: Session, skip: int = 0, limit: int = 100):
+def get_all_authors(db: Session, skip: int = 0, limit: int = 100) -> list[models.Author]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> Optional[models.Author]:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def check_authors_name(db: Session, author_name: str):
+def check_authors_name(db: Session, author_name: str) -> Optional[models.Author]:
     return (
         db.query(models.Author).filter(models.Author.name == author_name).first()
     )
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(
         name=author.name,
         biography=author.biography,
@@ -31,14 +32,14 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_all_books(db: Session, skip: int = 0, limit: int = 100):
+def get_all_books(db: Session, skip: int = 0, limit: int = 100) -> list[models.Book]:
     return db.query(models.Book).offset(skip).limit(limit).all()
 
 
 def filter_book_by_author(
         db: Session,
         author_id: int | None = None
-):
+) -> list[models.Book]:
     queryset = db.query(models.Book)
 
     if author_id:
@@ -47,7 +48,7 @@ def filter_book_by_author(
     return queryset.all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(
         title=book.title,
         summary=book.summary,
@@ -62,7 +63,7 @@ def create_book(db: Session, book: schemas.BookCreate):
     return db_book
 
 
-def create_author_book(db: Session, book: schemas.BookCreate, author_id: int):
+def create_author_book(db: Session, book: schemas.BookCreate, author_id: int) -> models.Book:
     db_book = models.Book(**book.dict(), author_id=author_id)
     db.add(db_book)
     db.commit()
--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def get_db() -> Session:
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"Hello": "World"}
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)) -> schemas.Author:
     db_author = crud.check_authors_name(db=db, author_name=author.name)
     if db_author:
         raise HTTPException(
@@ -37,13 +37,13 @@ def read_authors(
         skip: int = 0,
         limit: int = 100,
         db: Session = Depends(get_db)
-):
+) -> list[schemas.Author]:
     authors = crud.get_all_authors(db, skip=skip, limit=limit)
     return authors
 
 
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def retrieve_author(author_id: int, db: Session = Depends(get_db)):
+def retrieve_author(author_id: int, db: Session = Depends(get_db)) -> schemas.Author:
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author is not found")
@@ -53,18 +53,18 @@ def retrieve_author(author_id: int, db: Session = Depends(get_db)):
 @app.post("/authors/{author_id}/books/", response_model=schemas.Book)
 def create_book_for_author(
     author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
-):
+) -> schemas.Book:
     return crud.create_author_book(db=db, book=book, author_id=author_id)
 
 
 @app.get("/books/", response_model=list[schemas.Book])
-def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> list[schemas.Book]:
     books = crud.get_all_books(db, skip=skip, limit=limit)
     return books
 
 
 @app.get("/authors/{author_id}/books/", response_model=list[schemas.Book])
-def get_books_by_author(author_id: int, db: Session = Depends(get_db)):
+def get_books_by_author(author_id: int, db: Session = Depends(get_db)) -> list[schemas.Book]:
     db_books = crud.filter_book_by_author(db=db, author_id=author_id)
 
     if not db_books:
