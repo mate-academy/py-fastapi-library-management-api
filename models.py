7b24016b472253b78c6d63f133f88539b21e3fa6
Olexander Bryl <edlrian814@gmail.com>
2023-12-08 13:57:29 +0200

    Add models Book & Author

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Integer, Column, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Author(Base):
+    __tablename__ = "author"
+
+    id = Column(Integer, index=True, primary_key=True)
+    name = Column(String(255), unique=True, nullable=False)
+    bio = Column(String(511))
+
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "book"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    summary = Column(String(511), nullable=False)
+    publication_data = Column(Date)
+    author_id = Column(Integer, ForeignKey("author.id"))
+
+    author = relationship("Author", back_populates="books")
