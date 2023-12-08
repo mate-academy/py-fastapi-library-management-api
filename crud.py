36becea1810f7c018db8127415212aee527da750
Olexander Bryl <edlrian814@gmail.com>
2023-12-08 14:38:01 +0200

    Add crud funcs for Book

--- crud.py
+++ crud.py
@@ -22,3 +22,23 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
+def get_books(db: Session, author_id: int | None = None):
+    queryset = db.query(models.Book)
+
+    if author_id is not None:
+        queryset = queryset.filter(models.Book.author_id == author_id)
+
+    return queryset.all()
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.Book(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
