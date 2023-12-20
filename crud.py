7548846fb46183592fc3d603dc2502a98c499500
Olena Liubynetska <olena.liuby@gmail.com>
2023-12-20 20:01:19 +0000

    feat: Add create_new_book_with_author, get_book_by_title, get_all_books_with_pagination

--- crud.py
+++ crud.py
@@ -1,6 +1,9 @@
+from datetime import date
+
 from sqlalchemy.orm import Session
 
-from db.models import DBAuthor
+import schemas
+from db.models import DBAuthor, DBBook
 from schemas import AuthorCreate
 
 
@@ -24,3 +27,26 @@ def get_author_by_id(db: Session, author_id: int):
 
 def get_author_by_name(db: Session, name: str):
     return db.query(DBAuthor).filter(DBAuthor.name == name).first()
+
+
+def create_new_book_with_author(db: Session, book: schemas.BookCreate):
+    db_book = DBBook(
+        title=book.title,
+        summary=book.summary,
+        publication_date=book.publication_date,
+        author_id=book.author_id,
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+
+    return db_book
+
+
+def get_book_by_title(db: Session, title: str):
+    return db.query(DBBook).filter(DBBook.title == title).first()
+
+
+def get_all_books_with_pagination(db: Session, skip: int = 0, limit: int = 10):
+    books = db.query(DBBook).offset(skip).limit(limit).all()
+    return books
