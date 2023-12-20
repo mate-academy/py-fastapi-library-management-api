e16c20af55a0ae5503b366dc44f75c49ed524fc3
Olena Liubynetska <olena.liuby@gmail.com>
2023-12-20 20:01:51 +0000

    feat: Add books/ endpoint with get & post requests

--- main.py
+++ main.py
@@ -47,3 +47,26 @@ def read_single_author(author_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Author not found")
 
     return db_author
+
+
+@app.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    db_author = crud.get_author_by_id(db=db, author_id=book.author_id)
+    db_book = crud.get_book_by_title(db=db, title=book.title)
+
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author with this id isn't found")
+
+    if db_book is not None:
+        raise HTTPException(
+            status_code=400, detail="Book with this title already exists"
+        )
+
+    db_book = crud.create_new_book_with_author(db=db, book=book)
+    return db_book
+
+
+@app.get("/books/", response_model=list[schemas.Book])
+def read_all_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    books = crud.get_all_books_with_pagination(db=db, skip=skip, limit=limit)
+    return books
