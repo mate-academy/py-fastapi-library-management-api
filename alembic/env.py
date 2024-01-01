e4604487933bd0e9c0910bc6264048705a7c450d
sakhaline <alexandra.ivva@gmail.com>
2024-01-01 16:41:01 +0200

    Make author bio nullable

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
 
 from alembic import context
+
 from models import Base
 
 # this is the Alembic Config object, which provides
--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class Author(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False, unique=True, index=True)
-    bio = Column(String, nullable=False)
+    bio = Column(String)
 
     books = relationship("Book", back_populates="author")
 
