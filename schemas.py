f70c253871d322b67e50303b7a50714e9450437c
sakhaline <alexandra.ivva@gmail.com>
2024-01-02 19:50:29 +0200

    Update schemas

--- schemas.py
+++ schemas.py
@@ -13,7 +13,7 @@ class AuthorCreate(AuthorBase):
 
 
 class Author(AuthorBase):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict()
 
     id: int
 
@@ -33,7 +33,7 @@ class BookCreate(BookBase):
 
 
 class Book(BookBase):
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict()
 
     id: int
     author: Author
