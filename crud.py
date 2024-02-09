ab3bff337d9cf79c137d469bb6390f1615c95193
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-09 16:22:09 +0200

    Add a return type annotations for crud.py

--- crud.py
+++ crud.py
@@ -1,22 +1,24 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 import models
 import schemas
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> models.Author:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_author_by_name(db: Session, name: str):
+def get_author_by_name(db: Session, name: str) -> models.Author:
     return db.query(models.Author).filter(models.Author.name == name).first()
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 100):
+def get_authors(db: Session, skip: int = 0, limit: int = 100) -> List[models.Author]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(
         name=author.name,
         bio=author.bio,
@@ -27,11 +29,11 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 100):
+def get_books(db: Session, skip: int = 0, limit: int = 100) -> List[models.Book]:
     return db.query(models.Book).offset(skip).limit(limit).all()
 
 
-def create_author_book(db: Session, book: schemas.BookCreate):
+def create_author_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(
         **book.dict(),
     )
@@ -41,5 +43,5 @@ def create_author_book(db: Session, book: schemas.BookCreate):
     return db_book
 
 
-def get_books_by_author_id(db: Session, author_id: int):
+def get_books_by_author_id(db: Session, author_id: int) -> List[models.Book]:
     return db.query(models.Book).filter(models.Book.author_id == author_id)
