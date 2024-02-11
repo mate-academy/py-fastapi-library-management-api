517b00c5b680936b67850dd12965078ed8efb1fc
Maksym Bezhevets <begik4@gmail.com>
2024-02-11 21:30:06 +0200

    Fixed annotations

--- crud.py
+++ crud.py
@@ -1,10 +1,14 @@
+from typing import List
+
 from sqlalchemy.orm import Session
 
 import models
 import schemas
 
 
-def get_all_author(db: Session, skip: int | None, limit: int | None):
+def get_all_author(
+        db: Session, skip: int | None, limit: int | None
+) -> List[models.Author]:
     queryset = db.query(models.Author)
     if skip:
         queryset = queryset.offset(skip)
@@ -13,13 +17,13 @@ def get_all_author(db: Session, skip: int | None, limit: int | None):
     return queryset.all()
 
 
-def get_author_by_id(db: Session, author_id: int):
+def get_author_by_id(db: Session, author_id: int) -> models.Author:
     return (
         db.query(models.Author).filter(models.Author.id == author_id).first()
     )
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(**author.model_dump())
     db.add(db_author)
     db.commit()
@@ -30,7 +34,7 @@ def create_author(db: Session, author: schemas.AuthorCreate):
 
 def get_all_book(
     skip: int | None, limit: int | None, author_id: int | None, db: Session
-):
+) -> List[models.Book]:
     queryset = db.query(models.Book)
     if author_id:
         queryset = queryset.filter(models.Book.author_id == author_id)
@@ -41,7 +45,7 @@ def get_all_book(
     return queryset.all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(**book.model_dump())
     db.add(db_book)
     db.commit()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -20,7 +20,7 @@ def get_db() -> Session:
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"Hello": "World"}
 
 
@@ -29,12 +29,14 @@ def read_author(
     skip: int | None = None,
     limit: int | None = None,
     db: Session = Depends(get_db),
-):
+) -> List[schemas.Author]:
     return crud.get_all_author(db=db, skip=skip, limit=limit)
 
 
 @app.get("/authors/{author_id}", response_model=schemas.Author)
-def read_single_author(author_id: int, db: Session = Depends(get_db)):
+def read_single_author(
+        author_id: int, db: Session = Depends(get_db)
+) -> schemas.Author:
     db_author = crud.get_author_by_id(db=db, author_id=author_id)
     if db_author is None:
         raise HTTPException(
@@ -44,7 +46,9 @@ def read_single_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(
+        author: schemas.AuthorCreate, db: Session = Depends(get_db)
+) -> schemas.Author:
     return crud.create_author(db=db, author=author)
 
 
@@ -54,12 +58,14 @@ def read_book(
     limit: int | None = None,
     author_id: int | None = None,
     db: Session = Depends(get_db),
-):
+) -> List[schemas.Book]:
     return crud.get_all_book(
         db=db, skip=skip, limit=limit, author_id=author_id
     )
 
 
 @app.post("/books/", response_model=schemas.Book)
-def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+def create_book(
+        book: schemas.BookCreate, db: Session = Depends(get_db)
+) -> schemas.Book:
     return crud.create_book(db=db, book=book)
