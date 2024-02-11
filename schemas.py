8a45e134ae0f2f6fe9f07676d40d75221cc20edc
Maksym Bezhevets <begik4@gmail.com>
2024-02-11 17:18:02 +0200

    Solution

---
+++ schemas.py
@@ -0,0 +1,38 @@
+import datetime
+
+from pydantic import BaseModel
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: datetime.date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author: AuthorCreate
+
+    class Config:
+        orm_mode = True
+
+
+class Author(AuthorBase):
+    id: int
+    books: list[BookBase]
+
+    class Config:
+        orm_mode = True
