2e57b17d00e0d990356b0f76c82e902be6e462dc
srx <srx92@protonmail.com>
2024-02-21 22:24:44 +0200

    Solution

---
+++ crud.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from models import Author, Book
+from schemas import AuthorCreate, BookCreate
+
+
+def get_author(db: Session, author_id: int):
+    return db.query(Author).filter(Author.id == author_id).first()
+
+
+def get_authors(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Author).offset(skip).limit(limit).all()
+
+
+def create_author(db: Session, author: AuthorCreate):
+    db_author = Author(**author.dict())
+    db.add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
+
+
+def get_book(db: Session, book_id: int):
+    return db.query(Book).filter(Book.id == book_id).first()
+
+
+def get_books(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Book).offset(skip).limit(limit).all()
+
+
+def create_book(db: Session, book: BookCreate):
+    db_book = Book(**book.dict())
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
