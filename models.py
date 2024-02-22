452fde4d334bb0ee89853196039efa0eeb9b4b02
Dmytry-S <sviridoffd@gmail.com>
2024-02-22 12:44:06 +0200

    Implement library management api task

---
+++ models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False, unique=True)
+    bio = Column(String(500))
+    books = relationship("Book", back_populates="author")
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), nullable=False)
+    summary = Column(String(500))
+    publication_date = Column(Date)
+    author_id = Column(Integer, ForeignKey("author.id"))
+    author = relationship("Author", back_populates="books")
+
+
---
+++ schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+
+
+class BookBase(BaseModel):
+    title: str
+    summary: str
+    publication_date: date
+
+
+class BookCreate(BookBase):
+    author_id: int
+
+
+class Book(BookBase):
+    id: int
+    author_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class AuthorBase(BaseModel):
+    name: str
+    bio: str
+
+
+class AuthorCreate(AuthorBase):
+    pass
+
+
+class Author(AuthorBase):
+    id: int
+    books: List[Book] = []
+
+    class Config:
+        orm_mode = True
