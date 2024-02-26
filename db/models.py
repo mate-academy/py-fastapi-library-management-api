fcfdd484e0ebe13aca1c9cc61e3d326ef0977d82
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-02-26 18:51:00 +0200

    deleted unuseless import

--- db/models.py
+++ db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import relationship, Mapped
+from sqlalchemy.orm import relationship
 
 from db.database import Base
 
