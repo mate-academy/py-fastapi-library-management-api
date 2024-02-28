2bcc6daf7a8b558a3be2a0502a31d9e2e7b6e071
Maxim V <m4k51m15k4m@gmail.com>
2024-02-28 21:13:32 +0200

    complete crud.py

--- crud.py
+++ crud.py
@@ -4,7 +4,6 @@ import models
 import schemas
 
 
-# Create Author
 def create_author(db: Session, author_create: schemas.AuthorCreate):
     db_author = models.DBAuthor(**author_create.dict())
     db.add(db_author)
@@ -13,17 +12,18 @@ def create_author(db: Session, author_create: schemas.AuthorCreate):
     return db_author
 
 
-# Get Author by ID
 def get_author(db: Session, author_id: int):
     return db.query(models.DBAuthor).filter(models.DBAuthor.id == author_id).first()
 
 
-# Get all Authors
+def get_author_by_name(db: Session, name: str):
+    return db.query(models.DBAuthor).filter(models.DBAuthor.name == name).first()
+
+
 def get_authors(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.DBAuthor).offset(skip).limit(limit).all()
 
 
-# Update Author
 def update_author(db: Session, author_id: int, author_update: schemas.AuthorCreate):
     db_author = get_author(db, author_id)
     for key, value in author_update.dict().items():
@@ -33,7 +33,6 @@ def update_author(db: Session, author_id: int, author_update: schemas.AuthorCrea
     return db_author
 
 
-# Delete Author
 def delete_author(db: Session, author_id: int):
     db_author = get_author(db, author_id)
     db.delete(db_author)
@@ -41,7 +40,6 @@ def delete_author(db: Session, author_id: int):
     return db_author
 
 
-# Create Book
 def create_book(db: Session, book_create: schemas.BookCreate):
     db_book = models.DBBook(**book_create.dict())
     db.add(db_book)
@@ -50,17 +48,18 @@ def create_book(db: Session, book_create: schemas.BookCreate):
     return db_book
 
 
-# Get Book by ID
 def get_book(db: Session, book_id: int):
     return db.query(models.DBBook).filter(models.DBBook.id == book_id).first()
 
 
-# Get all Books
+def get_books_by_author(db: Session, author_id: int):
+    return db.query(models.DBBook).filter(models.DBBook.author_id == author_id).all()
+
+
 def get_books(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.DBBook).offset(skip).limit(limit).all()
 
 
-# Update Book
 def update_book(db: Session, book_id: int, book_update: schemas.BookCreate):
     db_book = get_book(db, book_id)
     for key, value in book_update.dict().items():
@@ -70,7 +69,6 @@ def update_book(db: Session, book_id: int, book_update: schemas.BookCreate):
     return db_book
 
 
-# Delete Book
 def delete_book(db: Session, book_id: int):
     db_book = get_book(db, book_id)
     db.delete(db_book)
