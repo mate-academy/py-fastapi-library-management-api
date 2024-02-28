69bf411ba4c90926c9f1ffd961dc8c48d4c58a03
Maxim V <m4k51m15k4m@gmail.com>
2024-02-28 20:38:21 +0200

    fix models.py

--- models.py
+++ models.py
@@ -5,25 +5,31 @@ from database import Base
 
 
 class DBAuthor(Base):
-    __tablename__ = 'authors'
+    __tablename__ = "authors"
 
-    id = Column(Integer, primary_key=True, index=True, )
-    name = Column(String(63), nullable=False, unique=True, )
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+    name = Column(
+        String(63),
+        nullable=False,
+        unique=True,
+    )
     bio = Column(String)
-
-    # Define the one-to-many relationship with Book
-    books = relationship("Book", back_populates="author")
 
 
 class DBBook(Base):
-    __tablename__ = 'books'
+    __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(63), nullable=False, )
+    title = Column(
+        String(63),
+        nullable=False,
+    )
     summary = Column(String)
     publication_date = Column(Date)
-    author_id = Column(Integer, ForeignKey('authors.id'))
+    author_id = Column(Integer, ForeignKey("authors.id"))
 
-    # Define the many-to-one relationship with Author
-    author = relationship("Author", back_populates="books")
-
+    author = relationship(DBAuthor)
