7d38559f2ce339e30be2fd9b04eac0fe351fcff8
Maxim V <m4k51m15k4m@gmail.com>
2024-02-28 21:14:23 +0200

    main crud.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-import models, schemas, crud
-from database import  engine
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
 
-# Create database tables using SQLAlchemy models
 models.Base.metadata.create_all(bind=engine)
 
-# Define FastAPI application object
 app = FastAPI()
 
 
-# Dependency function to manage database sessions
 def get_db():
     db = SessionLocal()
     try:
@@ -20,21 +19,26 @@ def get_db():
         db.close()
 
 
-# API endpoints
+@app.get("/")
+def root() -> dict:
+    return {"message": "Hello World"}
 
-# Create a new author
+
 @app.post("/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db, author)
 
 
-# Retrieve a list of authors with pagination (skip, limit)
+@app.get("/books/", response_model=list[schemas.Book])
+def read_books(db: Session = Depends(get_db)):
+    return crud.get_books(db)
+
+
 @app.get("/authors/", response_model=list[schemas.Author])
 def get_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_authors(db, skip=skip, limit=limit)
 
 
-# Retrieve a single author by ID
 @app.get("/authors/{author_id}", response_model=schemas.Author)
 def get_author(author_id: int, db: Session = Depends(get_db)):
     db_author = crud.get_author(db, author_id)
@@ -43,19 +47,21 @@ def get_author(author_id: int, db: Session = Depends(get_db)):
     return db_author
 
 
-# Create a new book for a specific author
 @app.post("/authors/{author_id}/books/", response_model=schemas.Book)
-def create_book_for_author(author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)):
+def create_book_for_author(
+    author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
+):
     return crud.create_book(db, book, author_id)
 
 
-# Retrieve a list of books with pagination (skip, limit)
 @app.get("/books/", response_model=list[schemas.Book])
 def get_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_books(db, skip=skip, limit=limit)
 
 
-# Filter books by author ID
-@app.get("/books/author/{author_id}", response_model=list[schemas.Book])
-def get_books_by_author(author_id: int, db: Session = Depends(get_db)):
-    return crud.get_books_by_author(db, author_id)
+@app.get("/books/{author_id}", response_model=list[schemas.Book])
+def read_books(author_id: int, db: Session = Depends(get_db)):
+    books = crud.get_books_by_author(db, author_id)
+    if not books:
+        raise HTTPException(status_code=404, detail="Books not found")
+    return books
