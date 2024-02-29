c95c5a8c272bf0d2446422845c2c37385388276d
Maxim V <m4k51m15k4m@gmail.com>
2024-02-29 15:35:44 +0200

    fix: add return type annotations to main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-def get_db():
+def get_db() -> None:
     db = SessionLocal()
     try:
         yield db
@@ -25,22 +25,30 @@ def root() -> dict:
 
 
 @app.post("/authors/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(author: schemas.AuthorCreate,
+                  db: Session = Depends(get_db),
+                  ) -> models.DBAuthor:
     return crud.create_author(db, author)
 
 
 @app.get("/books/", response_model=list[schemas.Book])
-def read_books(db: Session = Depends(get_db)):
+def read_books(db: Session = Depends(get_db),
+               ) -> list[models.DBBook]:
     return crud.get_books(db)
 
 
 @app.get("/authors/", response_model=list[schemas.Author])
-def get_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def get_authors(skip: int = 0,
+                limit: int = 10,
+                db: Session = Depends(get_db),
+                ) -> list[models.DBAuthor]:
     return crud.get_authors(db, skip=skip, limit=limit)
 
 
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
-def get_author(author_id: int, db: Session = Depends(get_db)):
+def get_author(author_id: int,
+               db: Session = Depends(get_db),
+               ) -> models.DBAuthor:
     db_author = crud.get_author(db, author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -48,19 +56,25 @@ def get_author(author_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/authors/{author_id}/books/", response_model=schemas.Book)
-def create_book_for_author(
-    author_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
-):
+def create_book_for_author(author_id: int,
+                           book: schemas.BookCreate,
+                           db: Session = Depends(get_db),
+                           ) -> models.DBBook:
     return crud.create_book(db, book, author_id)
 
 
 @app.get("/books/", response_model=list[schemas.Book])
-def get_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def get_books(skip: int = 0,
+              limit: int = 10,
+              db: Session = Depends(get_db),
+              ) -> list[models.DBBook]:
     return crud.get_books(db, skip=skip, limit=limit)
 
 
 @app.get("/books/{author_id}/", response_model=list[schemas.Book])
-def read_books(author_id: int, db: Session = Depends(get_db)):
+def read_books(author_id: int,
+               db: Session = Depends(get_db),
+               ) -> list[models.DBBook]:
     books = crud.get_books_by_author(db, author_id)
     if not books:
         raise HTTPException(status_code=404, detail="Books not found")
