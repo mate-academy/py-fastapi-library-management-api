3fb65e936c7afbfecb24b7790d72413b5d367ebf
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-03-03 18:30:01 +0200

    Solution

---
+++ main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get('/')
+def root():
+    return {"messages": "Hello World!"}
+
+
+@app.post("/authors/", response_model=schemas.Author)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    return crud.create_author(db=db, author=author)
+
+
+@app.get("/authors/", response_model=List[schemas.Author])
+def get_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_authors(db=db, skip=skip, limit=limit)
+
+
+@app.get("/authors/{author_id}", response_model=schemas.Author)
+def get_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db=db, author_id=author_id)
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+    return db_author
+
+
+@app.post("/books/", response_model=schemas.Book)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
+
+
+@app.get("/books/", response_model=List[schemas.Book])
+def get_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_books(db=db, skip=skip, limit=limit)
+
+
+@app.get("/books/{author_id}", response_model=List[schemas.Book])
+def get_books_by_author(author_id: int, skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_books_by_author(db=db, author_id=author_id, skip=skip, limit=limit)
